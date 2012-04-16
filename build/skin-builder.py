0f6ee6180fbaca22a5fae79ebd4e8ac2551885b1
Pablo Schklowsky <pablo@jwplayer.com>
2012-04-16 10:09:28 +0000

    Branch for v6 dev work

---
+++ build/skin-builder.py
@@ -0,0 +1,25 @@
+import base64
+from xml.dom import minidom
+import re
+
+basePath = '../../../skins'
+skinName = 'five'
+skinPath = basePath + '/' + skinName + '/' + skinName + '.xml'
+skinFile = open(skinPath,'r')
+skin = minidom.parse(skinFile)
+components = skin.getElementsByTagName('component')
+for component in components:
+	componentName = component.attributes['name']
+	elements = component.getElementsByTagName('element')
+	for element in elements:
+		elementPath = basePath + '/' + skinName + '/' + componentName.value + '/' + element.attributes['src'].value
+		imageText = base64.b64encode(open(elementPath,'rb').read())
+		element.attributes['src'].value = 'data:image/png;base64,' + imageText
+skinText = '\''+skin.toxml()+'\''
+whiteSpace = re.compile('>(.*?)<', re.S)
+skinText = whiteSpace.sub('><', skinText)
+
+outputPath = skinPath = basePath + '/' + skinName + '/' + skinName + '-min.xml'
+outputFile = open(outputPath,'w')
+outputFile.write(skinText)
+outputFile.close()
