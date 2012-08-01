b8bde264aa9f5ceb0a460dde0320274c8c6a780a
Jeroen Wijering <jeroen@jwplayer.com>
2012-08-01 22:44:27 +0000

    added quality API / MP3 flash tests

---
+++ test/assets/primaryCookie.js
@@ -0,0 +1,34 @@
+
+/** get primary cookie. **/
+var primaryCookie = 'html5';
+var cookies = document.cookie.split(";");
+for (i=0; i < cookies.length; i++) {
+    var x = cookies[i].substr(0, cookies[i].indexOf("="));
+    var y = cookies[i].substr(cookies[i].indexOf("=") + 1);
+    x = x.replace(/^\s+|\s+$/g,"");
+    if (x == 'primaryCookie') {
+        primaryCookie = y;
+    }
+}
+setTimeout(addSwitch,10);
+
+
+
+/** Set primary cookie. **/
+function switchCookie() { 
+    if(primaryCookie == 'html5') { 
+        primaryCookie = 'flash';
+    } else { 
+        primaryCookie = 'html5'
+    }
+    document.cookie = "primaryCookie=" + primaryCookie;
+    window.location.reload();
+};
+
+/** Insert a switch. **/
+function addSwitch() {
+    var list = document.getElementsByTagName('div')[0];
+    var secondary = "flash";
+    if(primaryCookie == 'flash') { secondary = "html5"; }
+    list.innerHTML = '<a href="javascript:switchCookie()">switch primary to '+secondary+'</a>';
+};
