24b5b53037ee98d8dfa07afe4bff709a3665945a
Pablo Schklowsky <pablo@jwplayer.com>
2012-08-09 21:34:51 +0000

    URL test cases

--- bin-debug/jwplayer.html5.js
+++ bin-debug/jwplayer.html5.js
@@ -6,7 +6,7 @@
  */
 (function(jwplayer) {
 	jwplayer.html5 = {};
-	jwplayer.html5.version = '6.0.2360';
+	jwplayer.html5.version = '6.0.2363';
 })(jwplayer);/**
  * HTML5-only utilities for the JW Player.
  * 
