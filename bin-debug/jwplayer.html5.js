6c3dfd60f0461513e80949ed595b81510b7447b0
Pablo Schklowsky <pablo@jwplayer.com>
2012-09-28 22:36:33 +0000

    Test case for getters not in other test cases

--- bin-debug/jwplayer.html5.js
+++ bin-debug/jwplayer.html5.js
@@ -6,7 +6,7 @@
  */
 (function(jwplayer) {
 	jwplayer.html5 = {};
-	jwplayer.html5.version = '6.0.2596';
+	jwplayer.html5.version = '6.0.2597';
 })(jwplayer);/**
  * HTML5-only utilities for the JW Player.
  * 
