cfd7006477fe81a6c7876f937aa1571834759cf0
Pablo Schklowsky <pablo@jwplayer.com>
2012-10-03 16:12:37 +0000

    Reverted initial playlist event flow

--- bin-debug/jwplayer.js
+++ bin-debug/jwplayer.js
@@ -16,7 +16,7 @@ jwplayer = function(container) {
 	}
 };
 
-jwplayer.version = '6.0.2635';
+jwplayer.version = '6.0.2644';
 
 // "Shiv" method for older IE browsers; required for parsing media tags
 jwplayer.vid = document.createElement("video");
--- src/js/jwplayer.js
+++ src/js/jwplayer.js
@@ -8,7 +8,7 @@ jwplayer = function(container) {
 	}
 };
 
-jwplayer.version = '6.0.2635';
+jwplayer.version = '6.0.2644';
 
 // "Shiv" method for older IE browsers; required for parsing media tags
 jwplayer.vid = document.createElement("video");
