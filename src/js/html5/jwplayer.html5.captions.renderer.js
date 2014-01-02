1f473e00551a279ba632907ba1944185a5472d9c
Rob Walch <rob@jwplayer.com>
2014-01-02 19:55:58 +0000

    html5 optimizations (captions and thumbs)

--- src/js/html5/jwplayer.html5.captions.renderer.js
+++ src/js/html5/jwplayer.html5.captions.renderer.js
@@ -1,4 +1,7 @@
-(function(html5, utils) {
+(function(jwplayer) {
+    var html5 = jwplayer.html5,
+        utils = jwplayer.utils,
+        _style = utils.css.style;
 
     /** Component that renders the actual captions on screen. **/
     html5.captions.renderer = function(_options,_div) {
@@ -13,27 +16,19 @@
         _textContainer,
         /** Current actie captions entry. **/
         _current,
-        /** Height of a single line. **/
-        _line,
         /** Current video position. **/
         _position,
         /** Should the captions be visible or not. **/
         _visible = 'visible',
         /** Interval for resize. **/
-        _interval,
-        /** Previous font size. **/
-        _scale = 0;
+        _interval = -1;
 
 
         /** Hide the rendering component. **/
         this.hide = function() {
+            clearInterval(_interval);
             _style(_container, {display:'none'});
-            if (_interval) {
-                clearInterval(_interval);
-                _interval = null;
-            }
         };
-
 
         /** Assign list of captions to the renderer. **/
         this.populate = function(captions) {
@@ -42,18 +37,20 @@
             _select();
         };
 
-
         /** Render the active caption. **/
         function _render(html) {
             html = html || '';
             //hide containers before resizing
+            _visible = 'hidden';
             _style(_container, {
-                visibility: 'hidden'
+                visibility: _visible
             });
             //update text and resize after delay
             _textContainer.innerHTML = html;
-            _visible = html.length ? 'visible' : 'hidden';
-            setTimeout(_resize, 20);
+            if (html.length) {
+                _visible = 'visible';
+                setTimeout(_resize, 16);
+            }
         }
 
         /** Store new dimensions. **/
@@ -67,21 +64,19 @@
             if (_visible === 'visible') {
                 var width = _container.clientWidth,
                     scale = Math.pow(width/400, 0.6);
-                if (scale !== _scale) {
-                    var size = _options.fontSize * scale;
-                    _style(_textContainer, {
-                        maxWidth: width + 'px',
-                        fontSize: Math.round(size) + 'px',
-                        lineHeight: Math.round(size * 1.4) + 'px',
-                        padding: Math.round(1 * scale) + 'px ' + Math.round(8 * scale) + 'px'
+
+                var size = _options.fontSize * scale;
+                _style(_textContainer, {
+                    maxWidth: width + 'px',
+                    fontSize: Math.round(size) + 'px',
+                    lineHeight: Math.round(size * 1.4) + 'px',
+                    padding: Math.round(1 * scale) + 'px ' + Math.round(8 * scale) + 'px'
+                });
+                if (_options.windowOpacity) {
+                    _style(_captionsWindow, {
+                        padding: Math.round(5 * scale) + 'px',
+                        borderRadius: Math.round(5 * scale) + 'px'
                     });
-                    if (_options.windowOpacity) {
-                        _style(_captionsWindow, {
-                            padding: Math.round(5 * scale) + 'px',
-                            borderRadius: Math.round(5 * scale) + 'px'
-                        });
-                    }
-                    _scale = scale;
                 }
                 _style(_container, {
                     visibility: _visible
@@ -157,8 +152,8 @@
 
             _style(_textContainer, textStyle);
 
-            _container.appendChild(_captionsWindow);
             _captionsWindow.appendChild(_textContainer);
+            _container.appendChild(_captionsWindow);
             _div.appendChild(_container);
         }
 
@@ -178,20 +173,10 @@
         /** Show the rendering component. **/
         this.show = function() {
             _style(_container, {display:'block'});
-            if (!_interval) {
-                _interval = setInterval(_resize, 250);
-            }
             _resize();
+            clearInterval(_interval);
+            _interval = setInterval(_resize, 250);
         };
-
-        /** Apply CSS styles to elements. **/
-        function _style(div, styles) {
-            utils.foreach(styles, function(property, val) {
-                if (div.style[property] !== val) {
-                    div.style[property] = val;
-                }
-            });
-        }
 
         /** Update the video position. **/
         this.update = function(position) {
@@ -204,4 +189,4 @@
         _setup();
     };
 
-})(jwplayer.html5, jwplayer.utils);
+})(jwplayer);
--- src/js/html5/jwplayer.html5.thumbs.js
+++ src/js/html5/jwplayer.html5.thumbs.js
@@ -1,126 +1,115 @@
-(function(html5) {
+(function(jwplayer) {
 
-    var utils = jwplayer.utils,
-        events = jwplayer.events,
-        _css = utils.css;
-        
+	var html5 = jwplayer.html5,
+		utils = jwplayer.utils,
+		events = jwplayer.events,
+		_css = utils.css;
+		
 
-    /** Displays thumbnails over the controlbar **/
-    html5.thumbs = function(id) {
-        var _display,
-        	_image,
-        	_imageURL,
-        	_cues,
-        	_vttPath,
-        	_id = id,
+	/** Displays thumbnails over the controlbar **/
+	html5.thumbs = function(id) {
+		var _display,
+			_cues,
+			_vttPath,
+			_id = id,
+			_src,
+			_eventDispatcher = new events.eventdispatcher();
 
-            /** Event dispatcher for thumbnail events. **/
-            _eventDispatcher = new events.eventdispatcher();
+		utils.extend(this, _eventDispatcher);
 
-        utils.extend(this, _eventDispatcher);
+		_display = document.createElement("div");
+		_display.id = _id;
 
-        function _init() {
-            _display = document.createElement("div");
-            _display.id = _id;
-        }
-
-        function _loadVTT(vtt) {
-        	_css(_internalSelector(), {
-        		display: "none"
-        	});
-        	
-        	if (vtt) {
-            	_vttPath = vtt.split("?")[0].split("/").slice(0, -1).join("/");
-            	new jwplayer.parsers.srt(_vttLoaded, _vttFailed, true).load(vtt);
-        	}
-        }
-        
-        function _vttLoaded(data) {
-        	if (!utils.typeOf(data) == "array") {
-        		return _vttFailed("Invalid data");
-        	}
-        	_cues = data;
-
-        	_css(_internalSelector(), {
-        		display: "block"
-        	});
-        }
-        
-        function _vttFailed(error) {
-        	utils.log("Thumbnails could not be loaded: " + error);        
-        }
-        
-		function _internalSelector() {
-			return '#' + _id;
+		function _loadVTT(vtt) {
+			_css.style(_display, {
+				display: "none"
+			});
+			
+			if (vtt) {
+				_vttPath = vtt.split("?")[0].split("/").slice(0, -1).join("/");
+				new jwplayer.parsers.srt(_vttLoaded, _vttFailed, true).load(vtt);
+			}
 		}
-        
-        function _loadImage(url) {
-        	if (url.indexOf("://") < 0) url = _vttPath ? _vttPath + "/" + url : url;
-        	var hashIndex = url.indexOf("#xywh");
-        	if (hashIndex > 0) {
-        		try {
-            		var regEx = /(.+)\#xywh=(\d+),(\d+),(\d+),(\d+)/,
-	        			thumbParams = regEx.exec(url),
-	        			image = thumbParams[1],
-	        			x = thumbParams[2] * -1,
-	        			y = thumbParams[3] * -1,
-	        			width = thumbParams[4],
-	        			height = thumbParams[5];
-            		
-            		_css(_internalSelector(), {
-            			'background-image': image,
-            			'background-position': x + "px " + y + "px",
-            			width: width,
-            			height: height
-            		});
-        		} catch(e) {
-        			_vttFailed("Could not parse thumbnail");
-        		}
-        	} else {
-        		var image = new Image();
-        		image.addEventListener('load', _imageLoaded, false);
-        		image.src = url;
-        	}
-        	
-        } 
-        
-        function _imageLoaded(evt) {
-        	var image = evt.target;
+		
+		function _vttLoaded(data) {
+			if (utils.typeOf(data) !== "array") {
+				return _vttFailed("Invalid data");
+			}
+			_cues = data;
 
-    		_css(_internalSelector(), {
-    			'background-image': image.src,
-    			'background-position': "0 0",
-    			width: image.width,
-    			height: image.height
-    		});
-        	
-        }
-        
-        this.load = function(thumbsVTT) {
-        	_loadVTT(thumbsVTT);
-        }
-        
-        this.element = function() {
-            return _display;
-        }
-        
-        // Update display
-        // TODO: only load image if it's different from the last one
-        var _updateTimeline = this.updateTimeline = function(seconds) {
-        	var i = 0;
-        	if (!_cues) return; 
-            while(i < _cues.length && seconds > _cues[i].end) {
-            	i++;
-            }
-            if (i == _cues.length) i--;
-            if (_cues[i].text) {
-            	_loadImage(_cues[i].text);
-            }
-        };
-        
-        _init();
+			_css.style(_display, {
+				display: "block"
+			});
+		}
+		
+		function _vttFailed(error) {
+			utils.log("Thumbnails could not be loaded: " + error);        
+		}
+		
+		function _loadImage(url) {
+			// only load image if it's different from the last one
+			if (url !== _src) {
+				if (url.indexOf("://") < 0) url = _vttPath ? _vttPath + "/" + url : url;
+				var hashIndex = url.indexOf("#xywh");
+				if (hashIndex > 0) {
+					try {
+						var regEx = /(.+)\#xywh=(\d+),(\d+),(\d+),(\d+)/,
+							thumbParams = regEx.exec(url),
+							x = thumbParams[2] * -1,
+							y = thumbParams[3] * -1,
+							width = thumbParams[4],
+							height = thumbParams[5],
+							bgImage = thumbParams[1];
+						
+						_css.style(_display, {
+							'background-image': bgImage,
+							'background-position': x + "px " + y + "px",
+							width: width,
+							height: height
+						});
+					} catch(e) {
+						_vttFailed("Could not parse thumbnail");
+					}
+					
+				} else {
+					var image = new Image();
+					image.addEventListener('load', _imageLoaded, false);
+				}
+				_src = url;
+			}
+		} 
+		
+		function _imageLoaded(evt) {
+			var image = evt.target;
+			_css.style(_display, {
+				'background-image': image.src,
+				'background-position': "0 0",
+				width: image.width,
+				height: image.height
+			});
+		}
+		
+		this.load = function(thumbsVTT) {
+			_loadVTT(thumbsVTT);
+		};
+		
+		this.element = function() {
+			return _display;
+		};
+		
+		// Update display
+		this.updateTimeline = function(seconds) {
+			var i = 0;
+			if (!_cues) return; 
+			while(i < _cues.length && seconds > _cues[i].end) {
+				i++;
+			}
+			if (i == _cues.length) i--;
+			if (_cues[i].text) {
+				_loadImage(_cues[i].text);
+			}
+		};
+	};
 
-    };
 
-
-})(jwplayer.html5);
+})(jwplayer);
