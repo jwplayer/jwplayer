jwplayer/jwplayer

commit 8a6d22a471555d170764d410b6fd47c5e28bf39b

Special-case events.onSetupError callback to handle many setup errors.

src/js/embed/jwplayer.embed.js
@@ -29,6 +29,10 @@
             delete playerApi.config.aspectratio;
         }
 
+        if(_config.events && _config.events.onSetupError) {
+            playerApi.onSetupError(_config.events.onSetupError);
+        }
+
         var _container = document.createElement('div');
         _container.id = _oldContainer.id;
         _container.style.width = _width.toString().indexOf('%') > 0 ? _width : (_width + 'px');
@@ -102,8 +106,11 @@
             if (_pluginloader.getStatus() === utils.loaderstatus.COMPLETE) {
                 for (var mode = 0; mode < _config.modes.length; mode++) {
                     if (_config.modes[mode].type && embed[_config.modes[mode].type]) {
-                        var configClone = utils.extend({}, _config),
-                            embedder = new embed[_config.modes[mode].type](_container, _config.modes[mode],
+                        var configClone = utils.extend({}, _config);
+                        if(configClone.events && configClone.events.onSetupError) {
+                            delete configClone.events.onSetupError;
+                        }
+                        var embedder = new embed[_config.modes[mode].type](_container, _config.modes[mode],
                                 configClone, _pluginloader, playerApi);
 
                         if (embedder.supportsConfig()) {
@@ -144,6 +151,7 @@
             playerApi.dispatchEvent(events.JWPLAYER_ERROR, {
                 message: 'Could not load plugin: ' + evt.message
             });
+            _dispatchSetupError(evt.message, false);
         }
 
         function _sourceError(evt) {

src/js/api/jwplayer.api.js
@@ -452,7 +452,7 @@
                             }
                             fn.call(this, args);
                         } catch (e) {
-                            utils.log('There was an error calling back an event handler');
+                            utils.log('There was an error calling back an event handler', e);
                         }
                     }
                 }

commit 9184d62cdd92d0926d261f4da9ee98023ef0f973

Do not dispatch Setup Error when there is a plugin loading error (regression in  8a6d22a471555d170764d410b6fd47c5e28bf39b)

src/js/embed/jwplayer.embed.js
@@ -143,11 +143,9 @@
         }
 
         function _pluginError(evt) {
-            //_errorScreen('Could not load plugins: ' + evt.message);
             playerApi.dispatchEvent(events.JWPLAYER_ERROR, {
                 message: 'Could not load plugin: ' + evt.message
             });
-            _dispatchSetupError(evt.message, false);
         }
 
         function _sourceError(evt) {