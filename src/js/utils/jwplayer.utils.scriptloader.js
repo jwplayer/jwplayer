97a0d8fbc0d76b661c405b438185e2c5e8ce50e1
DonatoB <donato@jwplayer.com>
2014-12-19 11:30:38 -0500

    Early return to decrease nesting

--- src/js/utils/jwplayer.utils.scriptloader.js
+++ src/js/utils/jwplayer.utils.scriptloader.js
@@ -14,45 +14,48 @@
             _status = _loaderstatus.NEW;
 
         this.load = function() {
-            if (_status === _loaderstatus.NEW) {
-                // If we already have a scriptloader loading the same script, don't create a new one;
-                var sameLoader = utils.scriptloader.loaders[url];
-                if (sameLoader) {
-                    _status = sameLoader.getStatus();
-                    if (_status < 2) {
-                        // dispatch to this instances listeners when the first loader gets updates
-                        sameLoader.addEventListener(_events.ERROR, _sendError);
-                        sameLoader.addEventListener(_events.COMPLETE, _sendComplete);
-                        return;
-                    }
-                    // already errored or loaded... keep going?
-                }
-
-                var head = document.getElementsByTagName('head')[0] || document.documentElement;
-                var scriptTag = document.createElement('script');
-
-                var done = false;
-                scriptTag.onload = scriptTag.onreadystatechange = function(evt) {
-                    if (!done &&
-                       (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')) {
-                        done = true;
-                        _sendComplete(evt);
-
-                        // Handle memory leak in IE
-                        scriptTag.onload = scriptTag.onreadystatechange = null;
-                        if (head && scriptTag.parentNode) {
-                            head.removeChild(scriptTag);
-                        }
-                    }
-                };
-                scriptTag.onerror = _sendError;
-
-                scriptTag.src = url;
-                head.insertBefore(scriptTag, head.firstChild);
-
-                _status = _loaderstatus.LOADING;
-                utils.scriptloader.loaders[url] = this;
+            // Only execute on the first run
+            if (_status !== _loaderstatus.NEW) {
+                return;
             }
+
+            // If we already have a scriptloader loading the same script, don't create a new one;
+            var sameLoader = utils.scriptloader.loaders[url];
+            if (sameLoader) {
+                _status = sameLoader.getStatus();
+                if (_status < 2) {
+                    // dispatch to this instances listeners when the first loader gets updates
+                    sameLoader.addEventListener(_events.ERROR, _sendError);
+                    sameLoader.addEventListener(_events.COMPLETE, _sendComplete);
+                    return;
+                }
+                // already errored or loaded... keep going?
+            }
+
+            var head = document.getElementsByTagName('head')[0] || document.documentElement;
+            var scriptTag = document.createElement('script');
+
+            var done = false;
+            scriptTag.onload = scriptTag.onreadystatechange = function(evt) {
+                if (!done &&
+                   (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')) {
+                    done = true;
+                    _sendComplete(evt);
+
+                    // Handle memory leak in IE
+                    scriptTag.onload = scriptTag.onreadystatechange = null;
+                    if (head && scriptTag.parentNode) {
+                        head.removeChild(scriptTag);
+                    }
+                }
+            };
+            scriptTag.onerror = _sendError;
+
+            scriptTag.src = url;
+            head.insertBefore(scriptTag, head.firstChild);
+
+            _status = _loaderstatus.LOADING;
+            utils.scriptloader.loaders[url] = this;
         };
 
         function _sendError(evt) {
