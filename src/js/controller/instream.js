728d8f1ef62833898770f5798dd80383e68861f5
Rob Walch <rob@jwplayer.com>
2015-03-16 17:33:47 -0400

    removed custom instream view components. fixed html5 provider instream ad playback.

--- src/js/controller/instream.js
+++ src/js/controller/instream.js
@@ -1,17 +1,14 @@
 define([
     'utils/helpers',
-    'utils/css',
     'underscore',
     'utils/eventdispatcher',
     'events/events',
     'events/states',
     'controller/model',
-    'view/display',
-    'view/controlbar',
     'view/adskipbutton',
     'playlist/item'
-], function(utils, cssUtils, _, eventdispatcher,
-            events, states, Model, Display, Controlbar, Adskipbutton, PlaylistItem) {
+], function(utils, _, eventdispatcher,
+            events, states, Model, Adskipbutton, PlaylistItem) {
 
     var Instream = function(_controller, _model, _view) {
         var _defaultOptions = {
@@ -40,14 +37,10 @@ define([
             _olditem,
             _adModel,
             _currentProvider,
-            _cbar,
-            _instreamDisplay,
-            _instreamContainer,
             _completeTimeoutId = -1,
             _this = _.extend(this, new eventdispatcher());
 
         // Listen for player resize events
-        _controller.jwAddEventListener(events.JWPLAYER_RESIZE, _resize);
         _controller.jwAddEventListener(events.JWPLAYER_FULLSCREEN, _fullscreenHandler);
 
         /*****************************************
@@ -61,14 +54,11 @@ define([
             // Make sure the original player's provider stops broadcasting events (pseudo-lock...)
             _oldProvider = _model.getVideo();
 
-            var _video = _oldProvider.detachMedia();
+            // Was used to get video tag and store media state
+            _oldProvider.detachMedia();
 
             // Keep track of the original player state
-            if (_video) {
-                _oldpos = _oldProvider.currentTime;
-            } else {
-                _oldpos = _model.position;
-            }
+            _oldpos = _model.position;
 
             // Initialize the instream player's model copied from main player's model
             _adModel = new Model({
@@ -99,37 +89,9 @@ define([
                 _oldProvider.pause();
             }
 
-            // Instream display
-            _instreamDisplay = new Display(_view._skin, jwplayer(_controller.id), _adModel);
-            _instreamDisplay.forceState(states.BUFFERING);
-            // Create the container in which the controls will be placed
-            _instreamContainer = document.createElement('div');
-            _instreamContainer.id = _this.id + '_instream_container';
-            cssUtils.style(_instreamContainer, {
-                width: '100%',
-                height: '100%'
-            });
-
-            _instreamContainer.appendChild(_instreamDisplay.element());
-
-            // Instream controlbar
-            _cbar = new Controlbar(_view._skin, jwplayer(_controller.id), _adModel);
-            _cbar.instreamMode(true);
-            _instreamContainer.appendChild(_cbar.element());
-
-            if (_controller.jwGetControls()) {
-                _cbar.show();
-                _instreamDisplay.show();
-            } else {
-                _cbar.hide();
-                _instreamDisplay.hide();
-            }
-
             // Show the instream layer
-            _view.setupInstream(_instreamContainer, _cbar, _instreamDisplay, _adModel);
-
-            // Resize the instream components to the proper size
-            _resize();
+            _view.setupInstream(_adModel);
+            _view.forceState(states.BUFFERING);
 
             _this.jwInstreamSetText(_defaultOptions.loadingmessage);
         };
@@ -147,8 +109,8 @@ define([
                 index: _arrayIndex
             }, true);
 
-            var instreamLayer = _instreamContainer.parentNode;
-            var bottom = 10 + utils.bounds(instreamLayer).bottom - utils.bounds(_cbar.element()).top;
+            var controlbar = document.getElementById(_model.id + '_controlbar');
+            var bottom = 10 + utils.bounds(_view.controlsContainer()).bottom - utils.bounds(controlbar).top;
 
             // Copy the playlist item passed in and make sure it's formatted as a proper playlist item
             if (_.isArray(item)) {
@@ -166,6 +128,9 @@ define([
             // check provider after item change
             _checkProvider();
 
+            // Show the instream layer
+            _view.showInstream();
+
             _skipButton = new Adskipbutton(_controller.id, bottom, _options.skipMessage, _options.skipText);
             _skipButton.addEventListener(events.JWPLAYER_AD_SKIPPED, _skipAd);
             _skipButton.reset(_options.skipoffset || -1);
@@ -177,14 +142,14 @@ define([
                 _skipButton.hide();
             }
 
-
             var skipElem = _skipButton.element();
-            _instreamContainer.appendChild(skipElem);
+            _view.controlsContainer().appendChild(skipElem);
+
             // Match the main player's controls state
             _adModel.addEventListener(events.JWPLAYER_ERROR, errorHandler);
 
             // start listening for ad click
-            _instreamDisplay.setAlternateClickHandler(function(evt) {
+            _view.displayComp().setAlternateClickHandler(function(evt) {
                 evt = evt || {};
                 evt.hasControls = !!_controller.jwGetControls();
 
@@ -202,7 +167,7 @@ define([
             });
 
             if (utils.isMSIE()) {
-                _oldProvider.parentElement.addEventListener('click', _instreamDisplay.clickHandler);
+                _oldProvider.parentElement.addEventListener('click', _view.displayComp().clickHandler);
             }
 
             _view.addEventListener(events.JWPLAYER_AD_SKIPPED, _skipAd);
@@ -225,10 +190,33 @@ define([
             if (!_adModel) {
                 return;
             }
+
+            var skipElem = _skipButton.element();
+            _view.controlsContainer().removeChild(skipElem);
+
             _adModel.removeEventListener('fullscreenchange', _nativeFullscreenHandler);
             clearTimeout(_completeTimeoutId);
             _completeTimeoutId = -1;
-            _adModel.getVideo().detachMedia();
+
+            // We don't want the instream provider to be attached to the video tag anymore
+            _this.resetEventListeners();
+            if (_currentProvider) {
+                _currentProvider.detachMedia();
+                _currentProvider.resetEventListeners();
+                _currentProvider.destroy();
+            }
+            _adModel.resetEventListeners();
+
+            // Return the view to its normal state
+            _view.destroyInstream(_adModel.getVideo().isAudioFile());
+            if (_view.displayComp()) {
+                if (_oldProvider && _oldProvider.parentElement) {
+                    _oldProvider.parentElement.removeEventListener('click', _view.displayComp().clickHandler);
+                }
+                _view.displayComp().revertAlternateClickHandler();
+            }
+            _adModel = null;
+
             // Re-attach the controller
             _controller.attachMedia();
             // Load the original item into our provider, which sets up the regular player's video tag
@@ -241,61 +229,32 @@ define([
             } else {
                 _oldProvider.stop();
             }
-            _this.resetEventListeners();
 
-            // We don't want the instream provider to be attached to the video tag anymore
-
-            _adModel.getVideo().resetEventListeners();
-            _adModel.resetEventListeners();
-
-
-
-            // If we added the controlbar anywhere, let's get rid of it
-            if (_cbar) {
-                utils.tryCatch(function() {
-                    _cbar.element().parentNode.removeChild(_cbar.element());
-                });
-            }
-            if (_instreamDisplay) {
-                if (_oldProvider && _oldProvider.parentElement) {
-                    _oldProvider.parentElement.removeEventListener('click', _instreamDisplay.clickHandler);
-                }
-                _instreamDisplay.revertAlternateClickHandler();
-            }
             // Let listeners know the instream player has been destroyed, and why
             _sendEvent(events.JWPLAYER_INSTREAM_DESTROYED, {
                 reason: complete ? 'complete' : 'destroyed'
             }, true);
 
 
-
             if (_oldstate === states.PLAYING) {
                 // Model was already correct; just resume playback
                 _oldProvider.play();
             }
-
-            // Return the view to its normal state
-            _view.destroyInstream(_adModel.getVideo().isAudioFile());
-            _adModel = null;
         };
 
         /** Start instream playback **/
         _this.jwInstreamPlay = function() {
             //if (!_item) return;
             _adModel.getVideo().play(true);
-
-            // Show the instream layer
-            _view.showInstream();
+            _view.releaseControls();
+            _view.forceState(states.PLAYING);
         };
 
         /** Pause instream playback **/
         _this.jwInstreamPause = function() {
             //if (!_item) return;
             _adModel.getVideo().pause(true);
-            if (_controller.jwGetControls()) {
-                _instreamDisplay.show();
-                _cbar.show();
-            }
+            _view.forceState(states.PAUSED);
         };
 
         /** Seek to a point in instream media **/
@@ -306,7 +265,7 @@ define([
 
         /** Set custom text in the controlbar **/
         _this.jwInstreamSetText = function(text) {
-            _cbar.setText(text);
+            _view.setInstreamText(text);
         };
 
         _this.jwInstreamState = function() {
@@ -378,21 +337,10 @@ define([
             if (!_adModel) {
                 return;
             }
-            _resize();
-            if (!evt.fullscreen && utils.isIPad()) {
-                if (_adModel.state === states.PAUSED) {
-                    _instreamDisplay.show(true);
-                } else if (_adModel.state === states.PLAYING) {
-                    _instreamDisplay.hide();
-                }
-            }
         }
 
         /** Handle the JWPLAYER_MEDIA_BUFFER_FULL event **/
         function _bufferFullHandler() {
-            if (_instreamDisplay) {
-                _instreamDisplay.releaseState(_this.jwGetState());
-            }
             _adModel.getVideo().play();
         }
 
@@ -433,9 +381,7 @@ define([
         function _metaHandler(evt) {
             // If we're getting video dimension metadata from the provider, allow the view to resize the media
             if (evt.width && evt.height) {
-                if (_instreamDisplay) {
-                    _instreamDisplay.releaseState(_this.jwGetState());
-                }
+                _view.releaseState();
                 _view.resizeMedia();
             }
         }
@@ -446,16 +392,6 @@ define([
                 data.tag = _defaultOptions.tag;
             }
             _this.sendEvent(type, data);
-        }
-
-        // Resize handler; resize the components.
-        function _resize() {
-            if (_cbar) {
-                _cbar.redraw();
-            }
-            if (_instreamDisplay) {
-                _instreamDisplay.redraw();
-            }
         }
 
         _this.setControls = function(mode) {
