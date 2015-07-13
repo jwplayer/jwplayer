0f294cd7dddf869ce21001bbafbe76ffd159ee40
Rob Walch <rob@jwplayer.com>
2015-07-13 14:28:46 -0400

    Remove ie8 polyfills [#98868556]

--- test/config.js
+++ test/config.js
@@ -3,24 +3,27 @@ window.__BUILD_VERSION__ = 0;
 window.__FLASH_VERSION__ = 11.2;
 
 var base = '';
-var tests = [];
+var deps = [];
 var callback;
 
 if (!window.__karma__) {
     base = document.location.href.replace(/[^\/]+\/[^\/]*$/, '');
     // this path is relative to the baseUrl src/js folder
-    tests = ['../../test/tests'];
+    deps = ['../../test/tests'];
     callback = window.QUnit.start;
 
 } else {
     base = '/base/';
     for (var file in window.__karma__.files) {
         if (/test\/unit\/[^\/]+\.js$/.test(file)) {
-            tests.push(file);
+            deps.push(file);
         }
     }
     callback = window.__karma__.start;
 }
+
+// Add bind polyfill for phantomjs 1.x
+deps.unshift('bind-polyfill');
 
 // Add qunit-fixture to page if not present
 if (!document.getElementById('qunit-fixture')) {
@@ -53,6 +56,7 @@ require.config({
         'lessc':             components + '/require-less/lessc',
         'normalize':         components + '/require-less/normalize',
         'jquery':            components + '/jquery/dist/jquery',
+        'bind-polyfill':     components + '/bind-polyfill/index',
 
         // always use test/underscore in test scripts
         'test/underscore': components + '/underscore/underscore',
@@ -77,7 +81,7 @@ require.config({
     },
 
     // ask Require.js to load these files (all our tests)
-    deps: tests,
+    deps: deps,
 
     // start test run, once Require.js is done
     callback: callback
