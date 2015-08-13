018b504b99ac29680463a7608cb7d535eb081046
donato <donato@jwplayer.com>
2015-08-13 14:13:48 -0400

    Filter by first source instead of provider priority
    This also includes a change to the unit test which previously expected priority
    first filtering.

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,9 +1,10 @@
 define([
     'test/underscore',
+    'api/config',
     'controller/Setup',
     'controller/model',
     'events/events'
-], function (_, Setup, Model, events) {
+], function (_, Config, Setup, Model, events) {
     /* jshint qunit: true */
 
     module('Setup');
@@ -173,7 +174,7 @@ define([
 
     function getModel(config) {
         var m = new Model();
-        m.setup(config);
+        m.setup(new Config(config));
         return m;
     }
 });
