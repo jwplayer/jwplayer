652d8d71609054b676297bcf8925b52bf40a0613
Rob Walch <rob@jwplayer.com>
2015-10-06 14:08:06 -0400

    Trigger Flash blocked event during ads when provider is detached

--- src/js/providers/flash.js
+++ src/js/providers/flash.js
@@ -186,11 +186,11 @@ define([
                     // The browser may block the flash object until user enables it
                     var _this = this;
                     _flashBlockedTimeout = setTimeout(function() {
-                        _this.trigger('flashBlocked');
+                        Events.trigger.call(_this, 'flashBlocked');
                     }, 2000);
                     _swf.once('embedded', function() {
                         clearTimeout(_flashBlockedTimeout);
-                        this.trigger('flashUnblocked');
+                        Events.trigger.call(_this, 'flashUnblocked');
                     }, this);
 
                     // listen to events sendEvented from flash
@@ -317,10 +317,10 @@ define([
                         clearTimeout(_flashBlockedTimeout);
 
                         if (e.state === 'resume') {
-                            _this.trigger('flashUnblocked');
+                            Events.trigger.call(_this, 'flashUnblocked');
                         } else {
                             _flashBlockedTimeout = setTimeout(function () {
-                                _this.trigger('flashBlocked');
+                                Events.trigger.call(_this, 'flashBlocked');
                             }, 250);
                         }
                     }
