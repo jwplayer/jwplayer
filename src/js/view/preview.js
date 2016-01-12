4fd6c03fe5622ddf79453144164c79e1aa272ff6
pajong <jong@jwplayer.com>
2016-01-13 11:25:46 +1300

    Change the behavior to load  preview image
    We want to load preview image only when:
    1. Audio mode
    2. State is idle with autostart false
    3. State is idle with mobile
    4. State is complete on last playlist item
    
    JW7-1904

--- src/js/view/preview.js
+++ src/js/view/preview.js
@@ -1,12 +1,38 @@
 define([
-    'utils/underscore'
-], function(_) {
+    'utils/underscore',
+    'utils/helpers'
+], function(_, util) {
 
     var Preview = function(_model) {
         this.model = _model;
 
-        this.model.on('change:playlistItem', this.loadImage, this);
+        this.model.on('change:playlistItem', onPlaylistItem, this);
     };
+
+    function loadOnComplete() {
+        if (this.model.get('state') === 'complete') {
+            this.loadImage(this.model, this.model.get('playlistItem'));
+        }
+    }
+
+    function onPlaylistItem(model, playlistItem) {
+        var audio = (this.model.mediaModel.get('mediaType') === 'audio');
+        var idleManualStart = (this.model.get('state') === 'idle') && (!this.model.get('autostart'));
+        var idleMobile = (this.model.get('state') === 'idle') && (util.isMobile());
+        var endOfPlaylist = (this.model.get('item') === (this.model.get('playlist').length - 1));
+
+        this.model.off('change:state', loadOnComplete);
+        if (audio || idleManualStart || idleMobile) {
+            // load image if audio mode, or mobile/non-autostart with on idle state
+            this.loadImage(model, playlistItem);
+        } else {
+            this.el.style.backgroundImage = '';
+            if (endOfPlaylist) {
+                // if the last playlist item, load image on complete
+                this.model.on('change:state', loadOnComplete, this);
+            }
+        }
+    }
 
     _.extend(Preview.prototype, {
         setup: function(element) {
