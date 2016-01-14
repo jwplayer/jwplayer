179e153ccc215b61b5db9e2fb68a6c1f146e56c1
Evol Greaves <evol@jwplayer.com>
2016-01-14 17:09:16 -0500

    Set mediaType only for HLS in Safari

--- src/js/providers/html5.js
+++ src/js/providers/html5.js
@@ -1132,8 +1132,10 @@ define([
         }
 
         function _setMediaType(videoTracks) {
-            if(_levels && _levels.length && _levels[0].type === 'hls') {
-                var mediaType = videoTracks && videoTracks.length ? 'video' : 'audio';
+            // set mediaType only for HLS in Safari since the model is notified
+            // of the mediaType earlier for other formats.
+            if(videoTracks && _levels[0].type === 'hls') {
+                var mediaType = videoTracks.length ? 'video' : 'audio';
                 _this.trigger('mediaType', {mediaType: mediaType});
             }
         }
