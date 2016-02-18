3c4372fad8cbd0886304093f8beeb4d6dafc245f
Rob Walch <rob@jwplayer.com>
2016-02-18 11:36:44 -0500

    Remove element.getBoundingClientRect check http://caniuse.com/#search=getBoundingClientRect

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -121,31 +121,19 @@ define([
         if (!element || !document.body.contains(element)) {
             return bounds;
         }
-        if (element.getBoundingClientRect) {
-            var rect = element.getBoundingClientRect(element),
-                scrollOffsetY = window.pageYOffset,
-                scrollOffsetX = window.pageXOffset;
-            if (!rect.width && !rect.height && !rect.left && !rect.top) {
-                //element is not visible / no layout
-                return bounds;
-            }
-            bounds.left = rect.left + scrollOffsetX;
-            bounds.right = rect.right + scrollOffsetX;
-            bounds.top = rect.top + scrollOffsetY;
-            bounds.bottom = rect.bottom + scrollOffsetY;
-            bounds.width = rect.right - rect.left;
-            bounds.height = rect.bottom - rect.top;
-        } else {
-            /*jshint -W084 */ // For the while loop assignment
-            bounds.width = element.offsetWidth | 0;
-            bounds.height = element.offsetHeight | 0;
-            do {
-                bounds.left += element.offsetLeft | 0;
-                bounds.top += element.offsetTop | 0;
-            } while (element = element.offsetParent);
-            bounds.right = bounds.left + bounds.width;
-            bounds.bottom = bounds.top + bounds.height;
+        var rect = element.getBoundingClientRect(element),
+            scrollOffsetY = window.pageYOffset,
+            scrollOffsetX = window.pageXOffset;
+        if (!rect.width && !rect.height && !rect.left && !rect.top) {
+            //element is not visible / no layout
+            return bounds;
         }
+        bounds.left = rect.left + scrollOffsetX;
+        bounds.right = rect.right + scrollOffsetX;
+        bounds.top = rect.top + scrollOffsetY;
+        bounds.bottom = rect.bottom + scrollOffsetY;
+        bounds.width = rect.right - rect.left;
+        bounds.height = rect.bottom - rect.top;
         return bounds;
     };
 
