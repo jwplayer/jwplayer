83ddee7a5cb6ac2aa2abcf35895b10b73d45fdf5
Rik Heijdens <rik@jwplayer.com>
2016-03-08 15:30:20 +0100

    Improved file extension parsing

--- src/js/utils/strings.js
+++ src/js/utils/strings.js
@@ -57,9 +57,9 @@ define([
             return azureFormat;
         }
 
-        path = path.substring(path.lastIndexOf('/') + 1, path.length).split('?')[0].split('#')[0];
+        path = path.substring(path.lastIndexOf('.')).split('?')[0].split('#')[0];
         if (path.lastIndexOf('.') > -1) {
-            return path.substr(path.lastIndexOf('.') + 1, path.length).toLowerCase();
+          return path.substr(path.lastIndexOf('.') + 1, path.length).toLowerCase();
         }
     };
 
