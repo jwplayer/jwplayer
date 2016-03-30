de678f60fd9731cd785a622233265c8ce298ea7c
Rob Walch <rob@jwplayer.com>
2016-03-30 15:23:53 -0400

    Make flash and html5 providers external

--- src/js/providers/providers-loaded.js
+++ src/js/providers/providers-loaded.js
@@ -1,12 +1,5 @@
 define([
-    'providers/html5',
-    'providers/flash',
-], function(html5, flash) {
-
-    var Store = {
-        html5: html5,
-        flash: flash
-    };
-
-    return Store;
+], function() {
+    
+    return {};
 });
