3b80b1906d4a61e3582cdab29e611713587db3c6
Jong Park <jpark@JPark.office.longtailvideo.com>
2016-06-22 15:44:33 -0400

    Add mimeType to the sources
    We want to know what mimeType each of the sources are, so that we can display that for ad impression.
    JW7-1961

--- src/js/playlist/source.js
+++ src/js/playlist/source.js
@@ -27,14 +27,17 @@ define([
         // regex to check if mimetype is given
         var mimetypeRegEx = /^[^\/]+\/(?:x-)?([^\/]+)$/;
 
+        if (mimetypeRegEx.test(_source.type)) {
+            // if type is given as a mimetype
+            _source.mimeType = _source.type;
+            _source.type = _source.type.replace(mimetypeRegEx, '$1');
+        }
+
         // check if the source is youtube or rtmp
         if (utils.isYouTube(_source.file)) {
             _source.type = 'youtube';
         } else if (utils.isRtmp(_source.file)) {
             _source.type = 'rtmp';
-        } else if (mimetypeRegEx.test(_source.type)) {
-            // if type is given as a mimetype
-            _source.type = _source.type.replace(mimetypeRegEx, '$1');
         } else if (!_source.type) {
             _source.type = strings.extension(_source.file);
         }
