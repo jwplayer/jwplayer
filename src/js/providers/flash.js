249b71018ce3c427f042b84d4c0bdd75b5777dcc
Rob Walch <rob@jwplayer.com>
2016-06-27 15:16:04 -0400

    Sort levels for Flash in flash.js JW7-2607

--- src/js/providers/flash.js
+++ src/js/providers/flash.js
@@ -50,20 +50,6 @@ define([
 
         var _customLabels = _getCustomLabels();
 
-        /** Translate sources into quality levels, assigning custom levels if present. **/
-        function _labelLevels(levels) {
-            if (_customLabels) {
-                for (var i = 0; i < levels.length; i++) {
-                    var level = levels[i];
-                    if (level.bitrate) {
-                        // get label with nearest rate match
-                        var sourceKbps = Math.round(level.bitrate / 1000);
-                        level.label = _getNearestCustomLabel(sourceKbps);
-                    }
-                }
-            }
-        }
-
         function _getNearestCustomLabel(sourceKBps) {
             // get indexed value
             var label = _customLabels[sourceKBps];
@@ -127,11 +113,48 @@ define([
             removeBlockedCheck();
             checkFlashBlocked();
         }
-        
+
         function removeBlockedCheck() {
             clearTimeout(_flashBlockedTimeout);
             window.removeEventListener('focus', onFocus);
         }
+
+        function _updateLevelsEvent(e) {
+            var levels = e.levels;
+            for (var i = 0; i < levels.length; i++) {
+                var level = levels[i];
+                // Set original index
+                level.index = i;
+                // Translate sources into quality levels, assigning custom levels if present
+                if (_customLabels && level.bitrate) {
+                    // get label with nearest rate match
+                    var sourceKbps = Math.round(level.bitrate / 1000);
+                    level.label = _getNearestCustomLabel(sourceKbps);
+                }
+            }
+            e.levels =
+                _qualityLevels = _sortedLevels(e.levels);
+            e.currentQuality =
+                _currentQuality = _getSortedIndex(_qualityLevels, e.currentQuality);
+        }
+
+        function _sortedLevels(levels) {
+            return levels.sort(function(obj1, obj2) {
+                if (obj1.height && obj2.height) {
+                    return obj2.height - obj1.height;
+                }
+                return obj2.bitrate - obj1.bitrate;
+            });
+        }
+
+        function _getSortedIndex(levels, originalIndex) {
+            for (var i = 0; i < levels.length; i++) {
+                if (levels[i].index === originalIndex) {
+                    return  i;
+                }
+            }
+        }
+
 
         _.extend(this, Events, Tracks, {
                 init: function(item) {
@@ -280,19 +303,9 @@ define([
                     ];
 
                     // jwplayer 6 flash player events (forwarded from AS3 Player, Controller, Model)
-                    _swf.on(events.JWPLAYER_MEDIA_LEVELS, function(e) {
-                        _labelLevels(e.levels);
-                        _currentQuality = e.currentQuality;
-                        _qualityLevels = e.levels;
+                    _swf.on([events.JWPLAYER_MEDIA_LEVELS, events.JWPLAYER_MEDIA_LEVEL_CHANGED].join(' '), function(e) {
+                        _updateLevelsEvent(e);
                         this.trigger(e.type, e);
-                    }, this);
-
-                    _swf.on(events.JWPLAYER_MEDIA_LEVEL_CHANGED, function(e) {
-                        _labelLevels(e.levels);
-                        _currentQuality = e.currentQuality;
-                        _qualityLevels = e.levels;
-                        this.trigger(e.type, e);
-
                     }, this);
 
                     _swf.on(events.JWPLAYER_AUDIO_TRACKS, function(e) {
@@ -408,7 +421,7 @@ define([
                     return _fullscreen;
                 },
                 setCurrentQuality: function(quality) {
-                    _flashCommand('setCurrentQuality', quality);
+                    _flashCommand('setCurrentQuality', _qualityLevels[quality].index);
                 },
                 getCurrentQuality: function() {
                     return _currentQuality;
