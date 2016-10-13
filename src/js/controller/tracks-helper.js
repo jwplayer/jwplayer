6f0c45b1ede2115e8f24409d8ce089fdb2e47e89
John Bartos <jbartos@jwplayer.com>
2016-10-13 17:20:26 -0400

    Render natively with hlsjs

--- src/js/controller/tracks-helper.js
+++ src/js/controller/tracks-helper.js
@@ -24,8 +24,8 @@ define([
             return { label: label, unknownCount: unknownCount };
         },
         renderNatively: function (providerName) {
-            return (providerName === 'html5' || providerName === 'shaka' || providerName === 'caterpillar') &&
-                (browser.isChrome() || browser.isIOS() || browser.isSafari() || browser.isEdge());
+            return (providerName === 'html5' || providerName === 'shaka' || providerName === 'caterpillar' || providerName === 'hlsjs') &&
+                (browser.isChrome() || browser.isIOS() || browser.isSafari() || browser.isEdge() || (providerName === 'hlsjs' && browser.isFF()));
         }
     };
 });
