755ee79bfff437f2d3b98463905943ac850ea660
Matt Cosentino <mattcoz@gmail.com>
2016-11-15 18:46:57 -0600

    Fix default cue settings and use valid enum values

--- src/js/parsers/captions/vttparser.js
+++ src/js/parsers/captions/vttparser.js
@@ -173,7 +173,7 @@ define(['parsers/captions/vttcue'], function(VTTCue) {
                         vals = v.split(',');
                         settings.percent(k, vals[0]);
                         if (vals.length === 2) {
-                            settings.alt('positionAlign', vals[1], ['start', 'center', 'end']);
+                            settings.alt('positionAlign', vals[1], ['line-left', 'center', 'line-right', 'auto']);
                         }
                         break;
                     case 'size':
@@ -193,20 +193,8 @@ define(['parsers/captions/vttcue'], function(VTTCue) {
             cue.snapToLines = settings.get('snapToLines', true);
             cue.size = settings.get('size', 100);
             cue.align = settings.get('align', 'center');
-            cue.position = settings.get('position', {
-                start: 0,
-                left: 0,
-                center: 50,
-                end: 100,
-                right: 100
-            }, cue.align);
-            cue.positionAlign = settings.get('positionAlign', {
-                start: 'start',
-                left: 'start',
-                center: 'center',
-                end: 'end',
-                right: 'end'
-            }, cue.align);
+            cue.position = settings.get('position', 'auto');
+            cue.positionAlign = settings.get('positionAlign', 'auto');
         }
 
         function skipWhitespace() {
