3f32299184ca0659a002d76975a50619538b29d1
Evol Greaves <evol@jwplayer.com>
2016-12-05 18:32:33 -0500

    Hide the next button when there are no more items to play

--- src/js/view/next-display-icon.js
+++ src/js/view/next-display-icon.js
@@ -13,6 +13,9 @@ define([
         this.iconUI = new UI (this.el).on('click tap', function() {
             api.next();
         });
+        model.on('change:nextUp', function(model, nextUp) {
+            this.el.style.display = nextUp ? '' : 'none';
+        }, this);
     };
 
     _.extend(NextDisplayIcon.prototype, {
