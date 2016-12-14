9ccc3e510739045922aabc0d388e5ded7b16e847
Hussam Ghazzi <hussam@jwplayer.com>
2016-12-14 10:26:26 -0500

    cast button logic

--- src/js/view/controlbar.js
+++ src/js/view/controlbar.js
@@ -126,7 +126,7 @@ define([
                 mute: muteButton,
                 volume: volumeSlider,
                 volumetooltip: volumeTooltip,
-                cast: !utils.isSafari()? castButton(this._localization.cast) : button('jw-icon-airplay jw-off', this._api.castToggle, this._localization.airplay),
+                cast: utils.isChrome && !utils.isIOS()? castButton(this._localization.cast) : button('jw-icon-airplay jw-off', this._api.castToggle, this._localization.airplay),
                 fullscreen: button('jw-icon-fullscreen', this._api.setFullscreen, this._localization.fullscreen)
             };
 
