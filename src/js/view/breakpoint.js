e5955252b050fc4bfc12ac132e6ec010008b8bd1
Rob Walch <rob@jwplayer.com>
2017-01-18 14:00:48 -0500

    Manage player css flags - audio mode and time slider above are exclusive of each other - breakpoint < 2 gets time slider above.

--- src/js/view/breakpoint.js
+++ src/js/view/breakpoint.js
@@ -3,29 +3,30 @@ define([
     'utils/underscore',
 ], function (utils) {
     return function setBreakpoint(playerElement, playerWidth, playerHeight) {
-        var className = 'jw-breakpoint-';
         var width = playerWidth;
         var height = playerHeight;
 
+        var breakPoint = 0;
         if (width >= 1280) {
-            className += '7';
+            breakPoint = 7;
         } else if (width >= 960) {
-            className += '6';
+            breakPoint = 6;
         } else if (width >= 800) {
-            className += '5';
+            breakPoint = 5;
         } else if (width >= 640) {
-            className += '4';
+            breakPoint = 4;
         } else if (width >= 540) {
-            className += '3';
+            breakPoint = 3;
         } else if (width >= 420) {
-            className += '2';
+            breakPoint = 2;
         } else if (width >= 320) {
-            className += '1';
-        } else {
-            className += '0';
+            breakPoint = 1;
         }
 
+        var className = 'jw-breakpoint-' + breakPoint;
         utils.replaceClass(playerElement, /jw-breakpoint-\d+/, className);
         utils.toggleClass(playerElement, 'jw-orientation-portrait', (height > width));
+
+        return breakPoint;
     };
 });
