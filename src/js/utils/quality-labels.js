61280d5b8ee95e23d8857eb9b4ffa15d11e8cb41
Hussam Ghazzi <hussam@jwplayer.com>
2017-02-03 18:16:25 -0500

    missing comma

--- src/js/utils/quality-labels.js
+++ src/js/utils/quality-labels.js
@@ -74,6 +74,6 @@ define([
         createLabel: createLabel,
         getCustomLabel: getCustomLabel,
         findClosestBandwidth: findClosestBandwidth,
-        toKbps: toKbps
+        toKbps: toKbps,
     };
 });
