68a85417bd39cb1462ed53917e6aca0ca7a21c4e
Rob Walch <rob@jwplayer.com>
2017-03-08 08:54:00 -0500

    Avoid removing classes on view setup

--- src/js/view/components/volumetooltip.js
+++ src/js/view/components/volumetooltip.js
@@ -3,13 +3,13 @@ define([
     'view/components/slider',
     'utils/ui',
     'utils/helpers'
-], function(Tooltip, Slider, UI, utils) {
+], function(Tooltip, Slider, UI) {
     return Tooltip.extend({
         constructor: function(_model, name, ariaText) {
             this._model = _model;
 
             // Prevent Volume tooltip button from being aria-hidden="true"
-            Tooltip.call(this, name, ariaText, true);
+            Tooltip.call(this, name, ariaText, true, true);
 
             this.volumeSlider = new Slider('jw-slider-volume jw-volume-tip', 'vertical');
             this.addContent(this.volumeSlider.element());
@@ -17,8 +17,6 @@ define([
             this.volumeSlider.on('update', function (evt) {
                 this.trigger('update', evt);
             }, this);
-
-            utils.removeClass(this.el, 'jw-hidden');
 
             new UI(this.el, { useHover: true, directSelect: true })
                 .on('click', this.toggleValue, this)
