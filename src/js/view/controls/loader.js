5da6867301f8885936809da130fe7f8538e45c15
John Bartos <jbartos@jwplayer.com>
2017-03-15 15:48:20 -0400

    Implement babel preprocessor, switch to AMD modules

--- src/js/view/controls/loader.js
+++ src/js/view/controls/loader.js
@@ -1,14 +1,23 @@
-let controls = null;
+define([
 
-export function load() {
-    if (controls) {
-        return Promise.resolve(controls);
+], function() {
+    let controls = null;
+
+    function load() {
+        if (controls) {
+            return Promise.resolve(controls);
+        }
+
+        return new Promise(function (resolve) {
+            require.ensure(['view/controls/controls.js'], function (require) {
+                controls = require('view/controls/controls.js');
+                resolve(controls);
+            }, 'jwplayer.controls');
+        });
     }
 
-    return new Promise(function (resolve) {
-        require.ensure(['view/controls/controls.js'], function (require) {
-            controls = require('view/controls/controls.js').default;
-            resolve(controls);
-        }, 'jwplayer.controls');
-    });
-}
+    return {
+        load: load
+    };
+});
+
