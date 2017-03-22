4db9b347564905238e6f5a11df3f6a89ed069711
John Bartos <jbartos@jwplayer.com>
2017-03-22 15:59:48 -0400

    Improve mock ensure

--- test/mock/mock-ensure.js
+++ test/mock/mock-ensure.js
@@ -2,16 +2,24 @@ define([
     'providers/html5',
     'providers/flash',
     'polyfills/vtt',
-    'intersection-observer'
-], function (providerHtml5, providerFlash, vtt, intersectionObserver) {
+    'intersection-observer',
+    'view/controls/controls'
+], function (providerHtml5, providerFlash, vtt, intersectionObserver, controls) {
     __webpack_require__.e = function (array, callback) {
         callback(function webpackRequire(modulePath) {
-            return ({
+            const module = ({
                 'providers/html5': providerHtml5,
                 'providers/flash': providerFlash,
                 'polyfills/vtt': vtt,
-                'intersection-observer': intersectionObserver
+                'intersection-observer': intersectionObserver,
+                'view/controls/controls': controls
             })[modulePath];
+
+            if (!module) {
+                console.log('Require.ensure mock could not find your module! If your tests are failing, add your module to mock-ensure.js');
+            }
+
+            return module;
         });
     };
 });
