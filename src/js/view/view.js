cb7a536af81355787c6c8046d426b1e31f5d20b0
Rob Walch <rob@jwplayer.com>
2017-05-22 17:01:00 -0400

    Prevent responsive update before view is setup and define captions render in view constructor JW7-4376

--- src/js/view/view.js
+++ src/js/view/view.js
@@ -63,8 +63,8 @@ define([
 
         const _preview = new Preview(_model);
         const _title = new Title(_model);
+        const _captionsRenderer = new CaptionsRenderer(_model);
 
-        let _captionsRenderer;
         let _logo;
 
         let _playerState;
@@ -166,6 +166,9 @@ define([
         }
 
         function _responsiveUpdate() {
+            if (!_this.isSetup) {
+                return;
+            }
             _this.updateBounds();
             _this.updateStyles();
             _this.checkResized();
@@ -314,7 +317,6 @@ define([
             _logo.on(events.JWPLAYER_LOGO_CLICK, _logoClickHandler);
 
             // captions rendering
-            _captionsRenderer = new CaptionsRenderer(_model);
             _captionsRenderer.setup(_playerElement.id, _model.get('captions'));
 
             // captions should be place behind controls, and not hidden when controls are hidden
