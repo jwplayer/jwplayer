6d00fa0110bf074af9c422fa2b3fbb12d49333e2
Karim Mourra <karim@jwplayer.com>
2017-07-07 11:49:13 -0400

    switched to ES5 syntax

--- src/js/compatibility.js
+++ src/js/compatibility.js
@@ -1,10 +1,10 @@
-((playerLibrary) => {
+(function (playerLibrary) {
 
     // Check if the version of the player requires the compatibility shim
     if (parseInt(playerLibrary.version, 10) >= 7) {
 
         // Redefine jwplayer global
-        window.jwplayer = (query) => {
+        window.jwplayer = function(query) {
 
             // Get JW Player 8 instance
             var playerInstance = playerLibrary(query);
@@ -13,104 +13,61 @@
             playerInstance.removeEventListener = playerInstance.off.bind(this);
             playerInstance.getItem = playerInstance.getPlaylistIndex;
             playerInstance.getMeta = playerInstance.getItemMeta;
-            playerInstance.getRenderingMode = () => {
+            playerInstance.getRenderingMode = function() {
                 return 'html5';
             };
 
             // Add deprecated API methods
-            let callbackMap = new Map([
-                ['onBuffer', 'buffer'],
-                ['onPause', 'pause'],
-                ['onPlay', 'play'],
-                ['onIdle', 'idle'],
-                ['onBufferChange', 'bufferChange'],
-                ['onBufferFull', 'bufferFull'],
-                ['onError', 'error'],
-                ['onSetupError', 'setupError'],
-                ['onFullscreen', 'fullscreen'],
-                ['onMeta', 'meta'],
-                ['onMute', 'mute'],
-                ['onPlaylist', 'playlist'],
-                ['onPlaylistItem', 'playlistItem'],
-                ['onPlaylistComplete', 'playlistComplete'],
-                ['onReady', 'ready'],
-                ['onResize', 'resize'],
-                ['onComplete', 'complete'],
-                ['onSeek', 'seek'],
-                ['onTime', 'time'],
-                ['onVolume', 'volume'],
-                ['onBeforePlay', 'beforePlay'],
-                ['onBeforeComplete', 'beforeComplete'],
-                ['onDisplayClick', 'displayClick'],
-                ['onControls', 'controls'],
-                ['onQualityLevels', 'levels'],
-                ['onQualityChange', 'levelsChanged'],
-                ['onCaptionsList', 'captionsList'],
-                ['onCaptionsChange', 'captionsChanged'],
-                ['onAdError', 'adError'],
-                ['onAdClick', 'adClick'],
-                ['onAdImpression', 'adImpression'],
-                ['onAdTime', 'adTime'],
-                ['onAdComplete', 'adComplete'],
-                ['onAdCompanions', 'adCompanions'],
-                ['onAdSkipped', 'adSkipped'],
-                ['onAdPlay', 'adPlay'],
-                ['onAdPause', 'adPause'],
-                ['onAdMeta', 'adMeta'],
-                ['onCast', 'cast'],
-                ['onAudioTrackChange', 'audioTrackChanged'],
-                ['onAudioTracks', 'audioTracks']
-            ]);
+            var callbackMap = {
+                onBuffer: 'buffer',
+                onPause: 'pause',
+                onPlay: 'play',
+                onIdle: 'idle',
+                onBufferChange: 'bufferChange',
+                onBufferFull: 'bufferFull',
+                onError: 'error',
+                onSetupError: 'setupError',
+                onFullscreen: 'fullscreen',
+                onMeta: 'meta',
+                onMute: 'mute',
+                onPlaylist: 'playlist',
+                onPlaylistItem: 'playlistItem',
+                onPlaylistComplete: 'playlistComplete',
+                onReady: 'ready',
+                onResize: 'resize',
+                onComplete: 'complete',
+                onSeek: 'seek',
+                onTime: 'time',
+                onVolume: 'volume',
+                onBeforePlay: 'beforePlay',
+                onBeforeComplete: 'beforeComplete',
+                onDisplayClick: 'displayClick',
+                onControls: 'controls',
+                onQualityLevels: 'levels',
+                onQualityChange: 'levelsChanged',
+                onCaptionsList: 'captionsList',
+                onCaptionsChange: 'captionsChanged',
+                onAdError: 'adError',
+                onAdClick: 'adClick',
+                onAdImpression: 'adImpression',
+                onAdTime: 'adTime',
+                onAdComplete: 'adComplete',
+                onAdCompanions: 'adCompanions',
+                onAdSkipped: 'adSkipped',
+                onAdPlay: 'adPlay',
+                onAdPause: 'adPause',
+                onAdMeta: 'adMeta',
+                onCast: 'cast',
+                onAudioTrackChange: 'audioTrackChanged',
+                onAudioTracks: 'audioTracks'
+            };
 
-//            var callbackMap = {
-//                onBuffer: 'buffer',
-//                onPause: 'pause',
-//                onPlay: 'play',
-//                onIdle: 'idle',
-//                onBufferChange: 'bufferChange',
-//                onBufferFull: 'bufferFull',
-//                onError: 'error',
-//                onSetupError: 'setupError',
-//                onFullscreen: 'fullscreen',
-//                onMeta: 'meta',
-//                onMute: 'mute',
-//                onPlaylist: 'playlist',
-//                onPlaylistItem: 'playlistItem',
-//                onPlaylistComplete: 'playlistComplete',
-//                onReady: 'ready',
-//                onResize: 'resize',
-//                onComplete: 'complete',
-//                onSeek: 'seek',
-//                onTime: 'time',
-//                onVolume: 'volume',
-//                onBeforePlay: 'beforePlay',
-//                onBeforeComplete: 'beforeComplete',
-//                onDisplayClick: 'displayClick',
-//                onControls: 'controls',
-//                onQualityLevels: 'levels',
-//                onQualityChange: 'levelsChanged',
-//                onCaptionsList: 'captionsList',
-//                onCaptionsChange: 'captionsChanged',
-//                onAdError: 'adError',
-//                onAdClick: 'adClick',
-//                onAdImpression: 'adImpression',
-//                onAdTime: 'adTime',
-//                onAdComplete: 'adComplete',
-//                onAdCompanions: 'adCompanions',
-//                onAdSkipped: 'adSkipped',
-//                onAdPlay: 'adPlay',
-//                onAdPause: 'adPause',
-//                onAdMeta: 'adMeta',
-//                onCast: 'cast',
-//                onAudioTrackChange: 'audioTrackChanged',
-//                onAudioTracks: 'audioTracks'
-//            };
-
-            callbackMap.forEach((value, name) => {
-                playerInstance[name] = (callback) => {
-                    playerInstance.on(value, callback);
+            Object.keys(callbackMap).forEach( function(key) {
+                playerInstance[key] = function (callback) {
+                    playerInstance.on(callbackMap[key], callback);
                 };
             });
+
             return playerInstance;
         }
     }
