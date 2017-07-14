a845cd1c65ab8197aa444d2f5c573bf849704c64
Karim Mourra <karim@jwplayer.com>
2017-07-14 16:09:02 -0400

    populated events with ES6 constants

--- src/js/events/events.js
+++ src/js/events/events.js
@@ -1,6 +1,361 @@
-//Template
+// Touch Events
+
+/**
+ * Event triggered when
+*/
+export const DRAG = 'drag';
+
+/**
+ * Event triggered when an ad is clicked
+*/
+export const DRAG_START = 'dragStart';
+
+/**
+ *
+*/
+export const DRAG_END = 'dragEnd';
+
+/**
+ *
+*/
+export const CLICK = 'click';
+
+/**
+ *
+*/
+export const DOUBLE_CLICK = 'doubleClick';
+
+/**
+ *
+*/
+export const TAP = 'tap';
+
+/**
+ *
+*/
+export const DOUBLE_TAP = 'doubleTap';
+
+/**
+ *
+*/
+export const OVER = 'over';
+
+/**
+ *
+*/
+export const MOVE = 'move';
+
+/**
+ *
+*/
+export const OUT = 'out';
+
+// Script Loaders
+
+/**
+ * Event triggered when
+*/
+export const COMPLETE = 'complete';
+
+/**
+ * Event triggered when an ad is clicked
+*/
+export const ERROR = 'error';
+
+// Ad events
 
 /**
  * Event triggered when an ad is clicked
 */
 export const AD_CLICK = 'adClick';
+
+/**
+ * Event triggered once an ad tag is loaded containing all companion creatives
+*/
+export const AD_COMPANIONS = 'adCompanions';
+
+/**
+ *
+*/
+export const AD_COMPLETE = 'adComplete';
+
+/**
+ *
+*/
+export const AD_ERROR = 'adError';
+
+/**
+ *
+*/
+export const AD_IMPRESSION = 'adImpression';
+
+/**
+ *
+*/
+export const AD_META = 'adMeta';
+
+/**
+ *
+*/
+export const AD_PAUSE = 'adPause';
+
+/**
+ *
+*/
+export const AD_PLAY = 'adPlay';
+
+/**
+ *
+*/
+export const AD_SKIPPED = 'adSkipped';
+
+/**
+ *
+*/
+export const AD_TIME = 'adTime';
+
+/**
+ *
+*/
+export const CAST_AD_CHANGED = 'castAdChanged';
+
+// Events
+
+/**
+ *
+*/
+export const MEDIA_COMPLETE = 'complete';
+
+/**
+ *
+*/
+export const READY = 'ready';
+
+/**
+ *
+*/
+export const MEDIA_SEEK = 'seek';
+
+/**
+ *
+*/
+export const MEDIA_BEFOREPLAY = 'beforePlay';
+
+/**
+ *
+*/
+export const MEDIA_BEFORECOMPLETE = 'beforeComplete';
+
+/**
+ *
+*/
+export const MEDIA_BUFFER_FULL = 'bufferFull';
+
+/**
+ *
+*/
+export const DISPLAY_CLICK = 'displayClick';
+
+/**
+ *
+*/
+export const PLAYLIST_COMPLETE = 'playlistComplete';
+
+/**
+ *
+*/
+export const CAST_SESSION = 'cast';
+
+/**
+ *
+*/
+export const MEDIA_ERROR = 'mediaError';
+
+/**
+ *
+*/
+export const MEDIA_FIRST_FRAME = 'firstFrame';
+
+/**
+ *
+*/
+export const MEDIA_PLAY_ATTEMPT = 'playAttempt';
+
+/**
+ *
+*/
+export const MEDIA_LOADED = 'loaded';
+
+/**
+ *
+*/
+export const MEDIA_SEEKED = 'seeked';
+
+// Setup Events
+
+/**
+ *
+*/
+export const SETUP_ERROR = 'setupError';
+
+// Utility
+
+/**
+ *
+*/
+export const ERROR = 'error';
+
+/**
+ *
+*/
+export const PLAYER_STATE = 'state';
+
+/**
+ *
+*/
+export const CAST_AVAILABLE = 'castAvailable';
+
+// Model Changes
+
+/**
+ *
+*/
+export const MEDIA_BUFFER = 'bufferChange';
+
+/**
+ *
+*/
+export const MEDIA_TIME = 'time';
+
+/**
+ *
+*/
+export const MEDIA_TYPE = 'mediaType';
+
+/**
+ *
+*/
+export const MEDIA_VOLUME = 'volume';
+
+/**
+ *
+*/
+export const MEDIA_MUTE = 'mute';
+
+/**
+ *
+*/
+export const MEDIA_META = 'meta';
+
+/**
+ *
+*/
+export const MEDIA_LEVELS = 'levels';
+
+/**
+ *
+*/
+export const MEDIA_LEVEL_CHANGED = 'levelsChanged';
+
+/**
+ *
+*/
+export const CONTROLS = 'controls';
+
+/**
+ *
+*/
+export const FULLSCREEN = 'fullscreen';
+
+/**
+ *
+*/
+export const RESIZE = 'resize';
+
+/**
+ *
+*/
+export const PLAYLIST_ITEM = 'playlistItem';
+
+/**
+ *
+*/
+export const PLAYLIST_LOADED = 'playlist';
+
+/**
+ *
+*/
+export const AUDIO_TRACKS = 'audioTracks';
+
+/**
+ *
+*/
+export const AUDIO_TRACK_CHANGED = 'audioTrackChanged';
+
+/**
+ *
+*/
+export const PLAYBACK_RATE_CHANGED = 'playbackRateChanged';
+
+// View Component Actions
+
+/**
+ *
+*/
+export const LOGO_CLICK = 'logoClick';
+
+// Model - Captions
+
+/**
+ *
+*/
+export const CAPTIONS_LIST = 'captionsList';
+
+/**
+ *
+*/
+export const CAPTIONS_CHANGED = 'captionsChanged';
+
+// Provider Communication
+
+/**
+ *
+*/
+export const PROVIDER_CHANGED = 'providerChanged';
+
+/**
+ *
+*/
+export const PROVIDER_FIRST_FRAME = 'providerFirstFrame';
+
+// UI Events
+
+/**
+ *
+*/
+export const USER_ACTION = 'userAction';
+
+/**
+ *
+*/
+export const PROVIDER_CLICK = 'providerClick';
+
+/**
+ *
+*/
+export const VIEW_TAB_FOCUS = 'tabFocus';
+
+/**
+ *
+*/
+export const CONTROLBAR_DRAGGING = 'scrubbing';
+
+/**
+ *
+*/
+export const INSTREAM_CLICK = 'instreamClick';
+
+/**
+ *
+*/
+export const BREAKPOINT = 'breakpoint'
--- src/js/events/states.js
+++ src/js/events/states.js
@@ -38,16 +38,3 @@ export const LOADING = 'loading';
  * Stalled State
 */
 export const STALLED = 'stalled';
-
-/*
-    BUFFERING: 'buffering',
-    IDLE: 'idle',
-    COMPLETE: 'complete',
-    PAUSED: 'paused',
-    PLAYING: 'playing',
-    ERROR: 'error',
-
-    // These exist at the provider level, but are converted to BUFFERING at higher levels
-    LOADING: 'loading',
-    STALLED: 'stalled'
-*/
