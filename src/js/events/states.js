e644a796674c9a99ec4e0209a6ba6cad3d17fd9d
Karim Mourra <karim@jwplayer.com>
2017-07-14 13:54:20 -0400

    added ES6 constants to states.js

--- src/js/events/states.js
+++ src/js/events/states.js
@@ -1,5 +1,53 @@
-define([], function() {
-    return {
 
-    };
-});
+/**
+ * Buffering State
+*/
+export const BUFFERING = 'buffering';
+
+/**
+ * Idle State
+*/
+export const IDLE = 'idle';
+
+/**
+ * Complete State
+*/
+export const COMPLETE = 'complete';
+
+/**
+ * Paused State
+*/
+export const PAUSED = 'paused';
+
+/**
+ * Playing State
+*/
+export const PLAYING = 'playing';
+
+/**
+ * Error State
+*/
+export const ERROR = 'error';
+
+/**
+ * Loading State
+*/
+export const LOADING = 'loading';
+
+/**
+ * Stalled State
+*/
+export const STALLED = 'stalled';
+
+/*
+    BUFFERING: 'buffering',
+    IDLE: 'idle',
+    COMPLETE: 'complete',
+    PAUSED: 'paused',
+    PLAYING: 'playing',
+    ERROR: 'error',
+
+    // These exist at the provider level, but are converted to BUFFERING at higher levels
+    LOADING: 'loading',
+    STALLED: 'stalled'
+*/
