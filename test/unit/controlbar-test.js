15cba4777c0039fcd1ecd38b16a537509f15cc43
Hussam Ghazzi <h.ghazzi@gmail.com>
2017-08-02 16:54:01 -0400

    Update Unit tests

--- test/unit/controlbar-test.js
+++ test/unit/controlbar-test.js
@@ -10,28 +10,35 @@ model.on = sinon.stub();
 model.on.returnsThis();
 model.set('localization', {});
 
-describe('Control Bar', () => {
+describe('Control Bar', function() {
 
     let controlBar;
     let container;
-    let buttons;
+    let spacer;
+    let children;
 
     beforeEach(() => {
-        controlBar = new ControlBar({}, model);
+        spacer = document.createElement('div');
+        spacer.className += 'jw-spacer';
+
         container = document.createElement('div');
-        controlBar.elements = { right: container };
-        buttons = controlBar.elements.right.children;
+        container.appendChild(document.createElement('div'));
+        container.appendChild(spacer);
+
+        controlBar = new ControlBar({}, model);
+        controlBar.el = container;
+        children = controlBar.el.children;
     });
 
-    describe('updateButtons', () => {
+    describe('updateButtons', function() {
 
-        it('should add nothing to the container if there are no buttons', () => {
+        it('should add nothing to the container if there are no buttons', function() {
             controlBar.updateButtons({});
 
-            expect(buttons.length).to.equal(0);
+            expect(children.length).to.equal(2);
         });
 
-        it('should removeButtons before adding any', () => {
+        it('should removeButtons before adding any', function() {
             controlBar.removeButtons = sinon.stub();
             container.insertBefore = sinon.spy();
 
@@ -40,56 +47,56 @@ describe('Control Bar', () => {
             expect(controlBar.removeButtons.calledBefore(container.insertBefore)).to.be.true;
         });
 
-        it('should add button to the container', () => {
+        it('should add button to the container', function() {
             controlBar.updateButtons(model, [{ id: '1' }]);
 
-            expect(buttons.length).to.equal(1);
+            expect(children.length).to.equal(3);
         });
 
-        it('should add buttons to the container in order', () => {
+        it('should add buttons to the container in order', function() {
             controlBar.updateButtons(model, [{ id: '1' }, { id: '2' }]);
 
-            expect(buttons.length).to.equal(2);
-            expect(buttons[0].getAttribute('button')).to.equal('1');
-            expect(buttons[1].getAttribute('button')).to.equal('2');
+            expect(children.length).to.equal(4);
+            expect(children[2].getAttribute('button')).to.equal('1');
+            expect(children[3].getAttribute('button')).to.equal('2');
         });
     });
 
-    describe('removeButtons', () => {
+    describe('removeButtons', function() {
 
-        it('should do nothing if there are no buttons in the container', () => {
+        it('should do nothing if there are no buttons in the container', function() {
             controlBar.removeButtons(container);
 
-            expect(buttons.length).to.equal(0);
+            expect(children.length).to.equal(2);
         });
 
-        it('should do nothing if there are buttons dont have the same ids', () => {
+        it('should do nothing if there are buttons that do have the same ids', function() {
             container.appendChild(document.createElement('div'));
             container.appendChild(document.createElement('div'));
             container.appendChild(document.createElement('div'));
 
-            buttons[0].setAttribute('button', '1');
-            buttons[1].setAttribute('button', '2');
-            buttons[2].setAttribute('button', '3');
+            children[2].setAttribute('button', '1');
+            children[3].setAttribute('button', '2');
+            children[4].setAttribute('button', '3');
 
             controlBar.removeButtons(container, [{ id: '4' }]);
 
-            expect(buttons.length).to.equal(3);
+            expect(children.length).to.equal(5);
         });
 
-        it('should remove buttons if there are buttons dont have the same ids', () => {
+        it('should remove buttons if there are button that dont have the same ids', function() {
             container.appendChild(document.createElement('div'));
             container.appendChild(document.createElement('div'));
             container.appendChild(document.createElement('div'));
 
-            buttons[0].setAttribute('button', '1');
-            buttons[1].setAttribute('button', '2');
-            buttons[2].setAttribute('button', '3');
+            children[2].setAttribute('button', '1');
+            children[3].setAttribute('button', '2');
+            children[4].setAttribute('button', '3');
 
             controlBar.removeButtons(container, [{ id: '1' }, { id: '2' }]);
 
-            expect(buttons.length).to.equal(1);
-            expect(buttons[0].getAttribute('button')).to.equal('3');
+            expect(children.length).to.equal(3);
+            expect(children[2].getAttribute('button')).to.equal('3');
         });
     });
 });
