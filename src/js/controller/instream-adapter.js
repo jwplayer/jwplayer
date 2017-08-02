704a2c4908212b673085fdcd07cb69065d513003
John Bartos <jbartos@jwplayer.com>
2017-08-02 17:29:09 -0400

    Ads mode controlbar JW8-209

--- src/js/controller/instream-adapter.js
+++ src/js/controller/instream-adapter.js
@@ -159,6 +159,8 @@ define([
         function _instreamTime(evt) {
             _instream._adModel.set('duration', evt.duration);
             _instream._adModel.set('position', evt.position);
+            _model.set('duration', evt.duration);
+            _model.set('position', evt.position);
         }
 
         function _instreamItemComplete(e) {
