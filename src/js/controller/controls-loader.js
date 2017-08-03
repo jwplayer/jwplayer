f21eaa6467d74e80ff2433205d709e9b65a7cb4b
Rob Walch <rob@jwplayer.com>
2017-08-03 14:44:41 -0400

    Improve handling of controls loading error JW8-112

--- src/js/controller/controls-loader.js
+++ src/js/controller/controls-loader.js
@@ -4,6 +4,9 @@ export function load() {
     if (!controlsPromise) {
         controlsPromise = require.ensure(['view/controls/controls'], function (require) {
             return require('view/controls/controls');
+        }, function() {
+            controlsPromise = null;
+            throw new Error('Network error');
         }, 'jwplayer.controls');
     }
     return controlsPromise;
