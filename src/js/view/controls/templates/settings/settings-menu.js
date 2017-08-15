7d38b8738c50cbbbe7185ef108ea84c347f79d41
John Bartos <jbartos@jwplayer.com>
2017-08-15 18:38:26 -0400

    Update template

--- src/js/view/controls/templates/settings/settings-menu.js
+++ src/js/view/controls/templates/settings/settings-menu.js
@@ -1,11 +1,11 @@
 export default () => {
     return (
-        `<div class="jw-reset  jw-settings-menu">` +
+        `<div class="jw-reset jw-settings-menu">` +
             `<div class="jw-reset jw-settings-topbar" role="menubar">` +
-                `<div class="jw-reset jw-settings-categories">` +
+                `<div class="jw-reset jw-settings-category">` +
                 `</div>` +
             `</div>` +
-            `<div class="jw-reset jw-settings-options">` +
+            `<div class="jw-reset jw-settings-content">` +
             `</div>` +
         `</div>`
     );
