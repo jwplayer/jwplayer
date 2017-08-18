6a6a98064a5a1067bd9a69abb1bd04b9cea5730c
Rob Walch <robwalch@users.noreply.github.com>
2017-08-18 11:02:06 -0400

    Fix "playReason" when autostart is on (#2245)
    Add a cancelable module used to handle checking autostart after the event loop is complete.
    
    Moves setting of "playAttempt" to true on the model until after "beforePlay" is triggered. Setting "playAttempt" to true triggers the start of playback if the current provider has fired "bufferFull".
    
    Clears the model's "playlistItem" attribute before setting it to ensure a change event is fired from the model, triggering "playlistItem" from the player api. (We used to fire this on "itemReady", but playlistItem should fire as soon as "playlistItem" is set and media model properties are reset).
    
    
    JW8-313
    JW8-312
    JW8-309

---
+++ src/js/utils/cancelable.js
@@ -0,0 +1,17 @@
+import { resolved } from 'polyfills/promise';
+
+export default function cancelable(callback) {
+    let cancelled = false;
+
+    return {
+        async: () => resolved.then(() => {
+            if (cancelled) {
+                return;
+            }
+            return callback();
+        }),
+        cancel: () => {
+            cancelled = true;
+        }
+    };
+}
