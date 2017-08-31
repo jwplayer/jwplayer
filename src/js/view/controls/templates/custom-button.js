b92f809544d350ad26a9a1a68a91332a61d454c1
Evol Greaves <evol@jwplayer.com>
2017-08-31 09:49:28 -0400

    Add tooltip for Fullscreen Icon Add button role and tabIndex to the correct element for custom buttons Add button type to buttons so they don’t cause a form to be submitted when clicked JW8-364

--- src/js/view/controls/templates/custom-button.js
+++ src/js/view/controls/templates/custom-button.js
@@ -6,11 +6,11 @@ export default (buttonClass = '', buttonId = '', image = '', ariaText) => {
     } else {
         const style = image ? `style="background-image: url(${image})"` : '';
         const imageClass = image ? 'jw-button-image' : 'jw-button-image';
-        const aria = ariaText ? `aria-label="${ariaText}" role="button" tabindex="0"` : '';
+        const aria = ariaText ? `aria-label="${ariaText}"` : '';
         imageDiv = `<div class="jw-icon ${imageClass} jw-button-color jw-reset" ${style} ${aria}></div>`;
     }
     return (
-        `<div class="jw-icon jw-icon-inline jw-button-color jw-reset ${buttonClass}" button="${buttonId}">
+        `<div class="jw-icon jw-icon-inline jw-button-color jw-reset ${buttonClass}" button="${buttonId}" role="button" tabindex="0">
             ${imageDiv}
         </div>`
     );
