e70cff10715b78e3519c6d6331ecb5ef6cde9c6e
Rob Walch <rob@jwplayer.com>
2017-09-13 17:31:35 -0400

    Remove media element parent setup (needs more work to manage player/element id and contains() logic for original element)

--- src/js/api/get-media-element.js
+++ src/js/api/get-media-element.js
@@ -2,11 +2,7 @@ export default function getMediaElement(parent) {
     // Find video tag, or create it if it doesn't exist.  View may not be built yet.
     let media = null;
     if (parent) {
-        if (parent.nodeName === 'VIDEO' || parent.nodeName === 'AUDIO') {
-            media = parent;
-        } else {
-            media = parent.querySelector('video, audio');
-        }
+        media = parent.querySelector('video, audio');
     }
     if (!media) {
         media = document.createElement('video');
