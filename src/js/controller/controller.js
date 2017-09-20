8183ee3a732fb0aab396991faf3fff18f74cf84d
Rob Walch <rob@jwplayer.com>
2017-09-20 17:49:10 -0400

    Detect interactions outside the player using window.event

--- src/js/controller/controller.js
+++ src/js/controller/controller.js
@@ -360,14 +360,14 @@ Object.assign(Controller.prototype, {
             return _model.get('state');
         }
 
-        function _play(meta = {}) {
+        function _play(meta) {
             checkAutoStartCancelable.cancel();
 
             if (_model.get('state') === STATE_ERROR) {
                 return resolved;
             }
 
-            const playReason = meta.reason;
+            const playReason = _getReason(meta);
             _model.set('playReason', playReason);
 
             const adState = _getAdState();
@@ -397,10 +397,23 @@ Object.assign(Controller.prototype, {
             return _model.playVideo(playReason);
         }
 
+        function _getReason(meta) {
+            if (!meta) {
+                if (_inInteraction(window.event)) {
+                    return 'interaction';
+                }
+                return 'external';
+            }
+            return meta.reason;
+        }
+
+        function _inInteraction(event) {
+            return event && /^(?:mouse|pointer|touch|gesture|click|key)/.test(event.type);
+        }
+
         function _primeMediaElementForPlayback() {
             // If we're in a user-gesture event call load() on video to allow async playback
-            const event = window.event;
-            if (event && /^(?:mouse|pointer|touch|gesture|click|key)/.test(event.type)) {
+            if (_inInteraction(window.event)) {
                 const mediaElement = _model.get('mediaElement');
                 if (!mediaElement.src) {
                     mediaElement.load();
@@ -448,13 +461,14 @@ Object.assign(Controller.prototype, {
             }
         }
 
-        function _pause(meta = {}) {
+        function _pause(meta) {
             _actionOnAttach = null;
             checkAutoStartCancelable.cancel();
 
-            _model.set('pauseReason', meta.reason);
+            const pauseReason = _getReason(meta);
+            _model.set('pauseReason', pauseReason);
             // Stop autoplay behavior if the video is paused by the user or an api call
-            if (meta.reason === 'interaction' || meta.reason === 'external') {
+            if (pauseReason === 'interaction' || pauseReason === 'external') {
                 _model.set('playOnViewable', false);
             }
 
