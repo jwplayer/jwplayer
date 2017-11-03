ed899327f0f13f9fdf8b8fd458fa8adf46583ffe
Hussam Ghazzi <h.ghazzi@gmail.com>
2017-11-03 18:17:34 -0400

    verify exception is caught

--- test/unit/trycatch-test.js
+++ test/unit/trycatch-test.js
@@ -16,14 +16,20 @@ describe('trycatch', function() {
         expect(value, 'returns value returned by function argument when no exception is thrown').to.equal(1);
     });
 
-    it('should throw custom Error on catch', function() {
+    it('should return JwError when there is an exception', function() {
         const error = new Error('Danger, Danger, Will Robinson!');
+        let exceptionThrown = false;
+        let jwError = null;
 
-        const jwError = tryCatch(function () {
-            throw error;
-        });
+        const trycatchThrow = function() {
+            jwError = tryCatch(function () {
+                exceptionThrown = true;
+                throw error;
+            });
+        };
 
-        expect(jwError.constructor.name).to.equal('JwError');
+        expect(trycatchThrow).to.not.throw();
+        expect(exceptionThrown).to.be.true;
         expect(jwError.message).to.equal('Danger, Danger, Will Robinson!');
     });
 
