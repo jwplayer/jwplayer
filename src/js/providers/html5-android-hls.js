d2600c1c58d36165cd48be5dc9912a0872adbe21
Hussam Ghazzi <h.ghazzi@gmail.com>
2017-11-03 16:39:52 -0400

    Make the hlsjsdefault flag be true by default
    JW8-264

--- src/js/providers/html5-android-hls.js
+++ src/js/providers/html5-android-hls.js
@@ -7,7 +7,7 @@ export function isAndroidHls(source) {
         }
 
         // Allow Android hls playback on versions 4.1 and above, excluding Firefox (which does not support HLS in any version)
-        return !Browser.firefox && parseFloat(OS.version.version) >= 4.1;
+        return !Browser.firefox && parseFloat(OS.version.version) >= 4.4;
     }
     return null;
 }
