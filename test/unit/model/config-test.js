903ebd09c4178fc12e0e1c2ac14f16fb87ee4f43
DanFerrer <danielferrer212@gmail.com>
2017-11-09 14:14:50 -0500

    create unit test for model.js

---
+++ test/unit/model/config-test.js
@@ -0,0 +1,28 @@
+import Model from 'controller/model';
+
+describe('Model', function() {
+    const config = {
+        file: '//playertest.longtailvideo.com/adaptive/bbbfull/bbbfull.m3u8',
+        image: 'http://d3el35u4qe4frz.cloudfront.net/bkaovAYt-480.jpg',
+        preload: 'auto'
+    };
+
+    let model;
+
+    before(function () {
+        model = new Model();
+        model.setup(config);
+    });
+
+    describe('return value', function() {
+        it.only('should return an object on setup', function() {
+            expect(model).to.be.an('object');
+        });
+    });
+
+    describe('state', function() {
+        it.only('should return an idle state', function() {
+            expect(model.get('state')).to.equal('idle');
+        });
+    });
+});
