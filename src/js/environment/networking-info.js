fcd674cfbed4f34a068d3c08f318925bcb50acaf
karimJWP <karimJWP@github.com>
2017-12-14 16:06:54 -0500

    extends navigator.connection

--- src/js/environment/networking-info.js
+++ src/js/environment/networking-info.js
@@ -1,13 +1,19 @@
-function NetworkingInfo () {
-    this.type = undefined;
-    this.effectiveType = undefined;
-    this.downlinkMax = 0;
-    this.downlink = 0;
-    this.rtt = 0;
-    this.saveData = false;
-    this.onLine = false;
-}
+const networkingDefaults = {
+    type: undefined,
+    effectiveType: undefined,
+    downlinkMax: 0,
+    downlink: 0,
+    rtt: 0,
+    saveData: false,
+    onLine: false
+};
 
-Object.assign(NetworkingInfo.prototype, navigator.connection);
+const networkingInfo = function (_) {
+    if (navigator && navigator.connection) {
+        return _.extend(networkingDefaults, navigator.connection);
+        //TODO: remove onchange, might not want to support on other browsers
+    }
+    return networkingDefaults;
+};
 
-export default NetworkingInfo;
+export default networkingInfo;
