1848ec1a759d1b920d3e0a8a056e1e9c31aa841d
karimJWP <karimJWP@github.com>
2017-12-14 16:54:55 -0500

    prevents overriding default network info

--- src/js/environment/networking-info.js
+++ src/js/environment/networking-info.js
@@ -10,13 +10,13 @@ const networkingDefaults = {
 
 const networkingInfo = function (_, provider) {
     if (navigator && navigator.connection) {
-        return _.extend(networkingDefaults, navigator.connection);
+        return _.extend({}, networkingDefaults, navigator.connection);
         // TODO: remove onchange, might not want to support on other browsers
     }
 
-    if (provider.name === 'hlsjs') {
+    if (provider && provider.name === 'hlsjs') {
         // TODO: obtain downlink from hls.js
-    } else if (provider.name === 'shaka') {
+    } else if (provider && provider.name === 'shaka') {
         // TODO: obtain downlink from shaka
     }
 
