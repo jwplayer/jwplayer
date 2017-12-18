5da8115cbf344b612bf631579975c26276be2101
DanFerrer <danielferrer212@gmail.com>
2017-12-18 16:29:51 -0500

    reset current index

--- src/js/view/controls/overlay-interface.js
+++ src/js/view/controls/overlay-interface.js
@@ -77,6 +77,7 @@ export default class OverlayInterface {
         }
 
         this.overlays = playlistItem.overlay;
+        this.currentIndex = 0;
         this.setOverlay(this.overlays[this.currentIndex]);
     }   
 
