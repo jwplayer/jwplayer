891c75b1aec932cf5acbe51770f3b54b72c55677
DanFerrer <danielferrer212@gmail.com>
2017-12-18 14:27:27 -0500

    add click method to trigger user function on overlay config

--- src/js/view/controls/overlay-interface.js
+++ src/js/view/controls/overlay-interface.js
@@ -1,5 +1,5 @@
 import overlayTemplate from 'view/controls/templates/overlay-interface';
-import { toggleClass } from 'utils/dom';
+import { addClass, removeClass } from 'utils/dom';
 import UI from 'utils/ui';
 import Events from 'utils/backbone.events';
 import utils from 'utils/helpers';
@@ -96,6 +96,11 @@ export default class OverlayInterface {
         }
     }
 
+    click() {
+        this.reset();
+        this.currentOverlay.action(this._api);
+    }
+
     addContent(elem) {
         if (this.content) {
             this.removeContent();
@@ -115,8 +120,14 @@ export default class OverlayInterface {
         return this.container;
     }
 
-    toggle() {
-
+    toggle(show) {
+        if(this.showing === show){
+            return;
+        }else if(show === true){
+            addClass(this.container, 'jw-interface-container-visible');
+        }else if(show === false){
+            removeClass(this.container, 'jw-interface-container-visible');
+        }
     }
 
     setOverlay(overlay) {
