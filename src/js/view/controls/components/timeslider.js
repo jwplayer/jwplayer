7f79d216d34c2919715ee44d3c88076e7cb8dd08
Alex Bussey <awbussey83@gmail.com>
2018-03-23 12:12:41 -0400

    Removed unneeded else block
    JW8-1352

--- src/js/view/controls/components/timeslider.js
+++ src/js/view/controls/components/timeslider.js
@@ -275,8 +275,6 @@ class TimeSlider extends Slider {
                 this.addCue(ele);
             });
             this.drawCues();
-        } else {
-            this.resetChapters();
         }
     }
 
