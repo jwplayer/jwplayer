af6bfa87ae4bbefff475e29314f6730eca6c59f3
John Kreitlow <jkreitlow@jwplayer.com>
2018-04-13 17:26:13 -0400

    refine sizing for right-click info overlay

--- src/js/view/controls/templates/info-overlay.js
+++ src/js/view/controls/templates/info-overlay.js
@@ -4,7 +4,7 @@ export default () => (
             `<div class="jw-reset jw-info-title"></div>` +
             `<div class="jw-reset jw-info-duration"></div>` +
             `<div class="jw-reset jw-info-description"></div>` +
-            `<div class="jw-reset jw-info-clientid"></div>` +
         `</div>` +
+        `<div class="jw-reset jw-info-clientid"></div>` +
     `</div>`
 );
