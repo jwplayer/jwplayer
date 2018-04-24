1f0d52c18ed67d61a69624e1ce2281b76c353d3b
Rob Walch <robwalch@users.noreply.github.com>
2018-04-24 18:42:07 -0400

    Fix setup tests and update playlist in setup after filtering (#2872)
    * Fix setup tests and update playlist in setup after filtering
    JW8-1447
    
    * Fix setup tests for IE11 by including polyfill in the page with karma

--- test/index.js
+++ test/index.js
@@ -1,7 +1,18 @@
+// Bundle files chunked by webpack
 import ProvidersLoaded from 'providers/providers-loaded';
+import * as vttparser from 'parsers/captions/vttparser';
 import * as html5 from 'providers/html5';
 import * as controls from 'view/controls/controls';
 import * as controller from 'controller/controller';
+
+// For unknown reasons importing xo causes webpack to split the chunks we need bundled
+// This polyfill will be embeded on the page by karma for IE
+// import xo from 'intersection-observer';
+
+// This should prevent us from attempting to load a chunk with 'intersection-observer'
+if (!('intersectionRatio' in window.IntersectionObserverEntry.prototype)) {
+    window.IntersectionObserverEntry.prototype.intersectionRatio = 1;
+}
 
 ProvidersLoaded.html5 = html5.default;
 
@@ -9,6 +20,7 @@ const testsContext = require.context('./unit', true);
 testsContext.keys().forEach(testsContext);
 
 export default [
+    vttparser,
     html5,
     controls,
     controller,
