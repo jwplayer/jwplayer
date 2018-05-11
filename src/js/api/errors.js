ee2279a7dfa0628ca43b60858b893f797db085ce
John Bartos <jbartos@jwplayer.com>
2018-05-11 14:34:50 -0400

    Address feedback

--- src/js/api/errors.js
+++ src/js/api/errors.js
@@ -40,17 +40,17 @@ export const SETUP_ERROR_LOADING_PLAYLIST = 102000;
 export const ERROR_LOADING_PLAYLIST = 202000;
 
 /**
- * @enum {ErrorCode} An error occurred duing Flash setup
+ * @enum {ErrorCode} An error occurred duing Flash setup.
  */
 export const FLASH_SETUP_ERROR = 210001;
 
 /**
- * @enum {ErrorCode} An error occurred during Flash playback
+ * @enum {ErrorCode} An error occurred during Flash playback.
  */
 export const FLASH_ERROR = 210000;
 
 /**
- * @enum {ErrorCode} A media error occurred during Flash playback
+ * @enum {ErrorCode} A media error occurred during Flash playback.
  */
 export const FLASH_MEDIA_ERROR = 214000;
 
