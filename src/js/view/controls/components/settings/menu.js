f7782943bbfbced5db58ef28ef9ffa8bb731ea08
DanFerrer <danielferrer212@gmail.com>
2018-05-17 17:10:00 -0400

    close menu on keydown when right arrow key is pressed

--- src/js/view/controls/components/settings/menu.js
+++ src/js/view/controls/components/settings/menu.js
@@ -34,7 +34,7 @@ export function SettingsMenu(onVisibility, onSubmenuAdded, onMenuEmpty) {
     const closeOnButton = function(evt) {
         // Close settings menu when enter is pressed on the close button
         // or when tab key is pressed since it is the last element in topbar
-        if (evt.keyCode === 13 || (evt.keyCode === 9 && !evt.shiftKey)) {
+        if (evt.keyCode === 13 || evt.keyCode === 39 || (evt.keyCode === 9 && !evt.shiftKey)) {
             instance.close(evt);
         }
     };
