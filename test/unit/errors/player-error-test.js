ebbddc48c85ae51d4266d8a67e8934e6534325ca
John Bartos <jbartos7@gmail.com>
2018-05-30 19:19:14 -0400

    Log errors via static PlayerError method, attach errorEvent listener to core model after initilization JW8-1542 (#2939)

--- test/unit/errors/player-error-test.js
+++ test/unit/errors/player-error-test.js
@@ -4,12 +4,12 @@ describe('PlayerError', function () {
     describe('get logMessage', function () {
         it('returns a string containing the code and a URL with the code appended as a hash', function () {
             const error = new PlayerError('', 123999);
-            expect(error.logMessage).to.equal(generateCopy(123999, 123999));
+            expect(PlayerError.logMessage(error.code)).to.equal(generateCopy(123999, 123999));
         });
 
         it('squishes the code hash when parsing a network error', function () {
             const error = new PlayerError('', 123404);
-            expect(error.logMessage).to.equal(generateCopy(123404, '123400-123599'));
+            expect(PlayerError.logMessage(error.code)).to.equal(generateCopy(123404, '123400-123599'));
         });
     });
 });
