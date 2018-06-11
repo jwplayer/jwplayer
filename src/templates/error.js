f8012c24d2041e999343916aa4df4cd5ab915dec
Rob Walch <rob@jwplayer.com>
2018-06-11 17:58:12 -0400

    Remove unused inline style in error template JW8-1395

--- src/templates/error.js
+++ src/templates/error.js
@@ -5,9 +5,8 @@ export default (id, message, errorCode, code) => {
             `<div class="jw-error-msg jw-info-overlay jw-reset">` +
                 `<style>` +
                 `[id="${id}"].jw-error{background:#000;overflow:hidden;position:relative}` +
-                `[id="${id}"] .jw-error-msg{top:50%;left:50%;position:absolute;align-items:center;transform:translate(-50%,-50%)}` +
-                `[id="${id}"] .jw-error-container{display:flex;flex-direction:column;align-items:center}` +
-                `[id="${id}"] .jw-error-text{color:#FFF;position:static;font:14px/1.35 Arial,Helvetica,sans-serif}` +
+                `[id="${id}"] .jw-error-msg{top:50%;left:50%;position:absolute;transform:translate(-50%,-50%)}` +
+                `[id="${id}"] .jw-error-text{color:#FFF;font:14px/1.35 Arial,Helvetica,sans-serif}` +
                 `</style>` +
                 `<div class="jw-icon jw-reset"></div>` +
                 `<div class="jw-info-container jw-reset">` +
