436feebc167f18af7cdda237b29c5d707b8549b9
Alex Bussey <awbussey83@gmail.com>
2018-06-25 12:19:36 -0400

    Added roundToHundredths math util
    JW8-1666

--- src/js/utils/math.js
+++ src/js/utils/math.js
@@ -3,3 +3,9 @@
 export const between = function (num, min, max) {
     return Math.max(Math.min(num, max), min);
 };
+
+// Round a given number to the hundredths-place
+// Used for playbackRates
+export const roundToHundredths = function (num) {
+    return parseFloat(num.toPrecision(2));
+};
