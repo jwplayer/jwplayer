3144f160096549fe0d0ef368f9f9614b164cadbd
Ian Boynton <ianboynton91@gmail.com>
2018-07-10 18:50:27 -0400

    Update controlbar.js
    Use single quotes

--- src/js/view/controls/controlbar.js
+++ src/js/view/controls/controlbar.js
@@ -59,7 +59,7 @@ function createCastButton(castToggle, localization) {
     const castButton = document.createElement('button', 'google-cast-button');
     setAttribute(castButton, 'type', 'button');
     setAttribute(castButton, 'tabindex', '-1');
-    castButton.className += " jw-reset";
+    castButton.className += ' jw-reset';
 
     const element = document.createElement('div');
     element.className = 'jw-reset jw-icon jw-icon-inline jw-icon-cast jw-button-color';
