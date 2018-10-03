159b65f5cb35bd233cbac6ee8d60c9bdf2e970e6
Jong Park <jong@jwplayer.com>
2018-10-03 13:48:55 -0400

    Check size of player when floating to determine the position

--- src/js/view/view.js
+++ src/js/view/view.js
@@ -847,8 +847,12 @@ function View(_api, _model) {
             style(_playerElement, {
                 backgroundImage: _preview.el.style.backgroundImage || _model.get('image')
             });
+            const breakpoint = getBreakpoint(rect.width);
+            const smallPlayer = breakpoint < 2;
+            toggleClass(_wrapperElement, 'jw-flag-small-player', smallPlayer);
 
             addClass(_playerElement, 'jw-flag-floating');
+
             _this.trigger(FLOAT, { floating: true });
             _model.set('floating', true);
 
@@ -865,6 +869,7 @@ function View(_api, _model) {
             floatingPlayer = null;
 
             removeClass(_playerElement, 'jw-flag-floating');
+            removeClass(_wrapperElement, 'jw-flag-small-player');
             _this.trigger(FLOAT, { floating: false });
             _model.set('floating', false);
 
