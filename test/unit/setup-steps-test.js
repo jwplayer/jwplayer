3a46a5213c4d30326931fc1d7d158393eb996b28
karimJWP <karimJWP@github.com>
2018-10-08 13:13:26 -0400

    verify loadTranslation is never rejected

--- test/unit/setup-steps-test.js
+++ test/unit/setup-steps-test.js
@@ -1,21 +1,19 @@
 import { loadTranslations } from 'api/setup-steps';
-import SimpleModel from 'model/simplemodel';
 import * as Language from 'utils/language';
 import { ERROR_LOADING_TRANSLATIONS, ERROR_LOADING_TRANSLATIONS_EMPTY_RESPONSE } from 'api/errors';
 import sinon from 'sinon';
 
-describe.only('Load Translations', function () {
+describe('Load Translations', function () {
     const sandbox = sinon.sandbox.create();
-    function MockModel() {}
-    Object.assign(MockModel.prototype, SimpleModel);
-    const model = new MockModel();
-    model.attributes = {
-        language: 'es-CL',
-        base: 'base',
-        setupConfig: {
-            localization: {}
-        },
-        intl: {}
+    const model = {
+        attributes: {
+            language: 'es-CL',
+            base: 'base',
+            setupConfig: {
+                localization: {}
+            },
+            intl: {}
+        }
     };
 
     beforeEach(function() {
@@ -27,19 +25,22 @@ describe.only('Load Translations', function () {
         sandbox.restore();
     });
 
+    function testLoadTranslations(expectedCode) {
+        loadTranslations(model).then(error => {
+            expect(error.code).to.equal(expectedCode);
+        }).catch(() => {
+            assert.isNotOk(true, 'loadTranslations should never be rejected')
+        });
+    }
+
     it('Triggers the translation load empty response warning when the response is empty', function() {
         sandbox.stub(Language, 'loadJsonTranslation').resolves({ response: null });
-        loadTranslations(model).then(error => {
-            expect(error.code).to.equal(ERROR_LOADING_TRANSLATIONS_EMPTY_RESPONSE);
-        });
+        testLoadTranslations(ERROR_LOADING_TRANSLATIONS_EMPTY_RESPONSE);
     });
 
     it('Triggers a warning composed of the translation loading base code and the ajax error code when ajax fails', function() {
-        const ajaxErrorCode = 999;
-        sandbox.stub(Language, 'loadJsonTranslation').rejects({ code: ajaxErrorCode });
-        loadTranslations(model).then(error => {
-            expect(error.code).to.equal(ERROR_LOADING_TRANSLATIONS + ajaxErrorCode);
-        });
-
+        const ERROR_AJAX = 999;
+        sandbox.stub(Language, 'loadJsonTranslation').rejects({ code: ERROR_AJAX });
+        testLoadTranslations(ERROR_LOADING_TRANSLATIONS + ERROR_AJAX);
     });
 });
