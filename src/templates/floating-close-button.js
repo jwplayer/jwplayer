d2ac327ddae30125f678e7fd05e5e1b1035a4d1e
vseventer <mark@vseventer.com>
2018-10-09 16:38:48 -0400

    [JW8-2286] Add jw-icon to float close.

--- src/templates/floating-close-button.js
+++ src/templates/floating-close-button.js
@@ -1,6 +1,6 @@
 export default (ariaLabel) => {
     return (
-        `<div class="jw-float-icon jw-reset" aria-label=${ariaLabel} tabindex="0">` +
+        `<div class="jw-float-icon jw-icon jw-reset" aria-label=${ariaLabel} tabindex="0">` +
         `</div>`
     );
 };
