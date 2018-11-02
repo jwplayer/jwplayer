30abb9b4b695d616212688ea9289e6193831c643
John Bartos <jbartos@jwplayer.com>
2018-11-02 16:51:06 -0400

    Avoid nulling originalContainer on playerDestroy to avoid null references when re-setupping the same instance

--- src/js/api/core-shim.js
+++ src/js/api/core-shim.js
@@ -177,7 +177,6 @@ Object.assign(CoreShim.prototype, {
         this._events =
             this._model =
             this.modelShim =
-            this.originalContainer =
             this.apiQueue =
             this.setup = null;
     },
