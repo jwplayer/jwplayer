016a14d5456882d26c902023c4b0ba2dce2066d2
karimJWP <karimJWP@github.com>
2018-11-16 13:32:18 -0500

    support error indentation

--- src/templates/error.js
+++ src/templates/error.js
@@ -6,11 +6,11 @@ export default (id, message, errorCode, code) => {
                 `<style>` +
                 `[id="${id}"].jw-error{background:#000;overflow:hidden;position:relative}` +
                 `[id="${id}"] .jw-error-msg{top:50%;left:50%;position:absolute;transform:translate(-50%,-50%)}` +
-                `[id="${id}"] .jw-error-text{color:#FFF;font:14px/1.35 Arial,Helvetica,sans-serif}` +
+                `[id="${id}"] .jw-error-text{text-align:start;color:#FFF;font:14px/1.35 Arial,Helvetica,sans-serif}` +
                 `</style>` +
                 `<div class="jw-icon jw-reset"></div>` +
                 `<div class="jw-info-container jw-reset">` +
-                    `<div class="jw-error-text jw-reset">${(message || '')}<span class="jw-break jw-reset"></span>${detail}</div>` +
+                    `<div class="jw-error-text jw-reset-text" dir="auto">${(message || '')}<span class="jw-break jw-reset"></span>${detail}</div>` +
                 `</div>` +
             `</div>` +
         `</div>`
