dd772cc9e36ba4ae36eec06a78616d69df216d72
Jong Park <jong@jwplayer.com>
2018-12-12 16:06:19 -0500

    Do not add focus outline on rightclick
    JW8-2515

--- src/js/utils/ui.js
+++ src/js/utils/ui.js
@@ -86,12 +86,16 @@ function initInteractionListeners(ui) {
     const listenerOptions = passiveEvents ? { passive } : false;
 
     const interactStartHandler = (e) => {
-        if (isRightClick(e)) {
-            return;
-        }
         const { target, type } = e;
         if (ui.directSelect && target !== el) {
             // The 'directSelect' parameter only allows interactions on the element and not children
+            return;
+        }
+
+        addClass(el, 'jw-no-focus');
+        ui.clickFocus = true;
+
+        if (isRightClick(e)) {
             return;
         }
 
@@ -130,9 +134,6 @@ function initInteractionListeners(ui) {
                 preventDefault(e);
             }
         }
-
-        addClass(el, 'jw-no-focus');
-        ui.clickFocus = true;
     };
 
     const interactDragHandler = (e) => {
--- src/js/view/controls/rightclick.js
+++ src/js/view/controls/rightclick.js
@@ -56,6 +56,7 @@ export default class RightClick {
 
     rightClick(evt) {
         this.lazySetup();
+        addClass(this.playerElement, 'jw-no-focus');
 
         if (this.mouseOverContext) {
             // right click on menu item should execute it
