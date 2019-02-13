6227cd876d9b5f6d12c5ddf20422ec1552dd7690
DanFerrer <danielferrer212@gmail.com>
2019-02-13 18:28:05 -0500

    remove call to play on seek

--- src/js/controller/controller.js
+++ src/js/controller/controller.js
@@ -611,10 +611,6 @@ Object.assign(Controller.prototype, {
             if (!_model.get('scrubbing') && state !== STATE_PLAYING) {
                 meta = meta || {};
                 meta.startTime = pos;
-                if (_model.get('streamType') === 'VOD' && (state === STATE_COMPLETE || _model.get('mediaModel').get('duration') - pos < 1)) {
-                    // Only resume VOD playback when  state is complete or position is within 1 second of duration.
-                    this.play(meta);
-                }
             }
         }
 
