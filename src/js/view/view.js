fad98d45fd24303e79ab10ed652014f1c081a3ca
Jong Park <jong@jwplayer.com>
2019-03-04 17:27:23 -0500

    Do not apply new max-width to floating player

--- src/js/view/view.js
+++ src/js/view/view.js
@@ -901,9 +901,7 @@ function View(_api, _model) {
         const width = _model.get('width');
         const height = _model.get('height');
         const styles = getPlayerSizeStyles(width);
-        if (isNumber(width)) {
-            styles.maxWidth = width;
-        }
+
         if (!_model.get('aspectratio')) {
             const containerWidth = _model.get('containerWidth');
             const containerHeight = _model.get('containerHeight');
