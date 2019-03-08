3c24bb58d91b1dd7ea69d6943deec40df55d684b
Zack Haigh <zack592@gmail.com>
2019-03-08 12:24:04 -0500

    'About This Video' right click option doesn't work on mobile JW8-5632

--- src/js/view/controls/templates/shortcuts-tooltip.js
+++ src/js/view/controls/templates/shortcuts-tooltip.js
@@ -1,16 +1,26 @@
-export default () => (
-    `<div class="jw-shortcuts-tooltip jw-modal jw-reset" title="Keyboard Shortcuts">` +
-        `<span class="jw-hidden" id="jw-shortcuts-tooltip-explanation">` +
-            `Press shift question mark to access a list of keyboard shortcuts` +
-        `</span>` +
-        `<div class="jw-reset jw-shortcuts-container">` +
-            `<div class="jw-reset jw-shortcuts-title">Keyboard Shortcuts</div>` +
-            `<div class="jw-reset jw-shortcuts-tooltip-list">` +
-                `<ul class="jw-shortcuts-tooltip-descriptions jw-reset jw-shortcuts-description">` +
-                `</ul>` +
-                `<ul class="jw-shortcuts-tooltip-keys jw-reset jw-shortcuts-description">` +
-                `</ul>` +
+export default (shortcuts = {}) => {
+    //  Iterate all shortcuts to create list of them. 
+    const shortcutsKey = (key) => `<li><span class="jw-hotkey">${key}</span></li>`;
+    const shortcutsDescription = (description) => `<li><span class="jw-hotkey-description">${description}</span></li>`;
+    let keyList = shortcuts.map(shortcut => shortcutsKey(shortcut.key));
+    let descriptionList = shortcuts.map(shortcut => shortcutsDescription(shortcut.description));
+
+    return (
+        `<div class="jw-shortcuts-tooltip jw-modal jw-reset" title="Keyboard Shortcuts">` +
+            `<span class="jw-hidden" id="jw-shortcuts-tooltip-explanation">` +
+                `Press shift question mark to access a list of keyboard shortcuts` +
+            `</span>` +
+            `<div class="jw-reset jw-shortcuts-container">` +
+                `<div class="jw-reset jw-shortcuts-title">Keyboard Shortcuts</div>` +
+                `<div class="jw-reset jw-shortcuts-tooltip-list">` +
+                    `<ul class="jw-shortcuts-tooltip-descriptions jw-reset jw-shortcuts-description">` +
+                        `${descriptionList.join('')}` +
+                    `</ul>` +
+                    `<ul class="jw-shortcuts-tooltip-keys jw-reset jw-shortcuts-description">` +
+                        `${keyList.join('')}` +
+                    `</ul>` +
                 `</div>` +
-        `</div>` +
-    `</div>`
-);
+            `</div>` +
+        `</div>`
+    );
+};
