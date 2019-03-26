d87fe46062c93bcae6dbbb0b066ad7d85b853044
Joe Natalzia <jnatalzia@jwplayer.com>
2019-03-26 13:26:12 -0400

    JW8-5775 Normalize the config in the model to ensure it is fully up to date before setup

--- test/unit/model/model-test.js
+++ test/unit/model/model-test.js
@@ -22,5 +22,43 @@ describe('Model', function() {
         it('should return an idle state', function() {
             expect(model.get('state')).to.equal('idle');
         });
+
+        it('should attempt to normalize the config', () => {
+            let tstModel = new Model();
+            const spyFunc = sinon.spy();
+            tstModel._normalizeConfig = spyFunc;
+
+            const tstCfg = {test: 'config'};
+            tstModel.setup(tstCfg);
+            expect(spyFunc.calledWith(tstCfg)).to.be.true;
+        });
+    });
+
+    describe('#_normalizeConfig', () => {
+        it('should leave the floating block untouched when disabled is not true', () => {
+            let startingCfg = {
+                floating: { disabled: false }
+            };
+            model._normalizeConfig(startingCfg);
+            expect(startingCfg.floating).to.deep.eq({ disabled: false });
+
+            startingCfg = {
+                floating: {}
+            };
+            model._normalizeConfig(startingCfg);
+            expect(startingCfg.floating).to.deep.eq({});
+        });
+        it('should null the floating block when disabled is true', () => {
+            let startingCfg = {
+                floating: {disabled: true}
+            };
+            model._normalizeConfig(startingCfg);
+            expect(startingCfg.floating).to.eq(null);
+        });
+        it('should null the floating block when it is not otherwise present', () => {
+            let startingCfg = {};
+            model._normalizeConfig(startingCfg);
+            expect(startingCfg.floating).to.eq(null);
+        });
     });
 });
