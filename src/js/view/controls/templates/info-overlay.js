1720faa2b1fff8340454b6ab5bef24a1d3b442dd
Zack Haigh <zack592@gmail.com>
2019-06-14 17:27:41 -0400

    Add rtl support to info-overlay and rightclick menu

--- src/js/view/controls/templates/info-overlay.js
+++ src/js/view/controls/templates/info-overlay.js
@@ -1,9 +1,9 @@
 export default () => (
     `<div class="jw-reset jw-info-overlay jw-modal">` +
         `<div class="jw-reset jw-info-container">` +
-            `<div class="jw-reset jw-info-title"></div>` +
-            `<div class="jw-reset jw-info-duration"></div>` +
-            `<div class="jw-reset jw-info-description"></div>` +
+            `<div class="jw-reset-text jw-info-title" dir="auto"></div>` +
+            `<div class="jw-reset-text jw-info-duration" dir="auto"></div>` +
+            `<div class="jw-reset-text jw-info-description" dir="auto"></div>` +
         `</div>` +
         `<div class="jw-reset jw-info-clientid"></div>` +
     `</div>`
