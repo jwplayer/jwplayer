0db9dca96184871f13c730ed37df91f56470fc53
Zack Haigh <zack592@gmail.com>
2019-06-14 17:27:41 -0400

    Add rtl support to info-overlay and rightclick menu

--- src/js/view/controls/templates/rightclick.js
+++ src/js/view/controls/templates/rightclick.js
@@ -16,10 +16,10 @@ const rightClickItem = (item, localization) => {
 };
 
 const itemContentTypes = {
-    link: ({ link, title, logo }) => `<a href="${link || ''}" class="jw-rightclick-link jw-reset" target="_blank" rel="noreferrer">${logo}${title || ''}</a>`,
-    info: (item, localization) => `<button type="button" class="jw-reset jw-rightclick-link jw-info-overlay-item">${localization.videoInfo}</button>`,
-    share: (item, localization) => `<button type="button" class="jw-reset jw-rightclick-link jw-share-item">${localization.sharing.heading}</button>`,
-    keyboardShortcuts: () => `<button type="button" class="jw-reset jw-rightclick-link jw-shortcuts-item">Keyboard Shortcuts</button>`,
+    link: ({ link, title, logo }) => `<a href="${link || ''}" class="jw-rightclick-link jw-reset-text" target="_blank" rel="noreferrer" dir="auto">${logo}${title || ''}</a>`,
+    info: (item, localization) => `<button type="button" class="jw-reset-text jw-rightclick-link jw-info-overlay-item" dir="auto">${localization.videoInfo}</button>`,
+    share: (item, localization) => `<button type="button" class="jw-reset-text jw-rightclick-link jw-share-item" dir="auto">${localization.sharing.heading}</button>`,
+    keyboardShortcuts: () => `<button type="button" class="jw-reset-text jw-rightclick-link jw-shortcuts-item" dir="auto">Keyboard Shortcuts</button>`,
 };
 
 
