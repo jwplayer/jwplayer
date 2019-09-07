b2cfb967decc8f98d6a699830cc3058851bff1f5
jnatalzia <jnatalzia@gmail.com>
2019-09-06 22:26:56 -0400

    Test the view manager

--- test/unit/view-manager-test.js
+++ test/unit/view-manager-test.js
@@ -1,4 +1,5 @@
 import ViewsManager from 'view/utils/views-manager';
+import sinon from 'sinon';
 
 describe('ViewsManager', function() {
     describe('#size', () => {
@@ -13,5 +14,44 @@ describe('ViewsManager', function() {
             ViewsManager.remove(view);
             expect(ViewsManager.size()).to.equal(0);
         });
+    });
+    describe('#addScrollHandler', () => {
+        let mock;
+        it('initializes the scroll handler and calls the added handler', () => {
+            mock = sinon.spy();
+            ViewsManager.addScrollHandler(mock);
+            
+            var event = document.createEvent('Event');
+            event.initEvent('scroll', true, true);
+            window.dispatchEvent(event);
+
+            expect(mock.called).to.be.true;
+        });
+        after(() => {
+            ViewsManager.removeScrollHandler(mock);
+        })
+    });
+    describe('#removeScrollHandler', () => {
+        let mock;
+        it('removes the expected handler', () => {
+            mock = sinon.spy();
+            ViewsManager.addScrollHandler(mock);
+            
+            var event = document.createEvent('Event');
+            event.initEvent('scroll', true, true);
+            window.dispatchEvent(event);
+
+            expect(mock.callCount).to.eql(1);
+
+            ViewsManager.removeScrollHandler(mock);
+
+            window.dispatchEvent(event);
+
+            expect(mock.callCount).to.eql(1);
+        });
+        after(() => {
+            // In case the test case breaks out, clean up
+            ViewsManager.removeScrollHandler(mock);
+        })
     });
 });
