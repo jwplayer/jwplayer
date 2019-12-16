988c30a78c84b45a3df02a9c7ab4b4691f958e4b
Zack Haigh <zack592@gmail.com>
2019-12-16 17:12:07 -0500

    Fix menu toggling behavior

--- src/js/view/controls/components/menu/menu.js
+++ src/js/view/controls/components/menu/menu.js
@@ -450,7 +450,10 @@ export default class Menu extends Events {
             }
         });
     }
-    toggle(evt) {
+    toggle(evt, isDefault) {
+        if (isDefault && this.mainMenu.visible) {
+            return this.mainMenu.close(evt);
+        }
         if (this.visible) {
             this.close(evt);
         } else {
