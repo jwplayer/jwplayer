0622d8ee5630cb8b0e5ebcfff33104f685c65b0d
Zack Haigh <zack592@gmail.com>
2019-12-16 15:50:44 -0500

    Fix quality menu rendering behavior for videos with multiple sources. Ensure all event listeners are removed

--- src/js/view/controls/components/menu/menu-item.js
+++ src/js/view/controls/components/menu/menu-item.js
@@ -8,6 +8,9 @@ export class MenuItem {
         this.ui = new UI(this.el).on('click tap enter', _action, this);
     }
     destroy() {
+        if (this.el.parentNode) {
+            this.el.parentNode.removeChild(this.el);
+        }
         this.ui.destroy();
     }
 }
@@ -17,11 +20,17 @@ export class RadioMenuItem extends MenuItem {
         super(_content, _action, _template);
     }
     activate() {
+        if (this.active) {
+            return;
+        }
         toggleClass(this.el, 'jw-settings-item-active', true);
         this.el.setAttribute('aria-checked', 'true');
         this.active = true;
     }
     deactivate() {
+        if (!this.active) {
+            return;
+        }
         toggleClass(this.el, 'jw-settings-item-active', false);
         this.el.setAttribute('aria-checked', 'false');
         this.active = false;
