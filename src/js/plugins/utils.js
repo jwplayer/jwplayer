847734f45a2c33185b23c71c524d3e79bd344b9a
vseventer <mark@vseventer.com>
2019-12-17 16:58:27 -0500

    [JW8-10769] Move plugin codes to commercial.

--- src/js/plugins/utils.js
+++ src/js/plugins/utils.js
@@ -4,39 +4,8 @@ export const getPluginName = function(url) {
     return url.replace(/^(.*\/)?([^-]*)-?.*\.(js)$/, '$2');
 };
 
-export function mapPluginToCode(pluginUrl) {
-    let code = 305000;
-    if (!pluginUrl) {
-        return code;
-    }
-
-    switch (getPluginName(pluginUrl)) {
-        case 'jwpsrv':
-            code = 305001;
-            break;
-        case 'googima':
-            code = 305002;
-            break;
-        case 'vast':
-            code = 305003;
-            break;
-        case 'freewheel':
-            code = 305004;
-            break;
-        case 'dai':
-            code = 305005;
-            break;
-        case 'gapro':
-            code = 305006;
-            break;
-        case 'bidding':
-            code = 305007;
-            break;
-        default:
-            break;
-    }
-
-    return code;
+export function mapPluginToCode(/* pluginUrl */) {
+    return 305000;
 }
 
 export function configurePlugin(pluginObj, pluginConfig, api) {
