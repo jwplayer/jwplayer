ce05d6bf88172ab7c5e8ab03e9f63a0ade50722d
Maria Maddox <mmaddox@jwplayer.com>
2020-01-22 09:39:59 -0500

    Add div wrapper to preview image to prevent overflow

--- src/js/view/preview.js
+++ src/js/view/preview.js
@@ -35,12 +35,18 @@ Object.assign(Preview.prototype, {
             image = this.image = new Image();
             image.src = img;
         }
-        style(this.el, {
-            backgroundImage: backgroundImage
-        });
        
         if (this.hasZoomThumbnail) {
+            this.imageEl = document.createElement('div');
+            style(this.imageEl, {
+                backgroundImage: backgroundImage
+            });
+            this.el.appendChild(this.imageEl);
             this.enableZoomThumbnail();     
+        } else {
+            style(this.el, {
+                backgroundImage: backgroundImage
+            });
         }
     },
     enableZoomThumbnail: function() {
@@ -50,17 +56,21 @@ Object.assign(Preview.prototype, {
         
         clearTimeout(this.zoomThumbnailTimeout);
         this.zoomThumbnailTimeout = setTimeout(() => {
-            this.el.classList.add('jw-ab-zoom-thumbnail');
-            this.el.style.transformOrigin = this.zoomOriginX + ' ' + this.zoomOriginY;
+            this.imageEl.classList.add('jw-ab-zoom-thumbnail');
+            this.imageEl.style.transformOrigin = this.zoomOriginX + ' ' + this.zoomOriginY;
         }, 2000);
     },
     pauseZoomThumbnail: function() {
         clearTimeout(this.zoomThumbnailTimeout);
-        this.el.style.animationPlayState = this.model.get('viewable') ? 'running' : 'paused';
+        if (this.imageEl) {
+            this.imageEl.style.animationPlayState = this.model.get('viewable') ? 'running' : 'paused';
+        }
     },
     removeZoomThumbnail: function() {
         clearTimeout(this.zoomThumbnailTimeout);
-        this.el.classList.remove('jw-ab-zoom-thumbnail');
+        if (this.imageEl) {
+            this.imageEl.classList.remove('jw-ab-zoom-thumbnail');
+        }
     },
     resize: function(width, height, stretching) {
         if (stretching === 'uniform') {
