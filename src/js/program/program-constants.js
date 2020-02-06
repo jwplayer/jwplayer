9735453b93e4974412178402ecfd1d95a0f99246
Rob Walch <rob@jwplayer.com>
2020-02-06 17:15:06 -0500

    Increase background loading start from 2 to 5 seconds before video completes JW8-10910

--- src/js/program/program-constants.js
+++ src/js/program/program-constants.js
@@ -2,6 +2,6 @@
 export const MEDIA_POOL_SIZE = 4;
 // The number of seconds before a BGL trigger at which we should start background loading. This ensures that we have
 // kicked off background loading before being able to transition to that item
-export const BACKGROUND_LOAD_OFFSET = 2;
+export const BACKGROUND_LOAD_OFFSET = 5;
 // The minimum time from the start of a video in which we can background load
 export const BACKGROUND_LOAD_MIN_OFFSET = 1;
