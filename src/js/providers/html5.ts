8a69bc591725ae45c635f31b568a135071f56a64
Maria Maddox <mmaddox@jwplayer.com>
2020-03-04 15:29:39 -0500

    Clean up files from rebase

--- src/js/providers/html5.ts
+++ src/js/providers/html5.ts
@@ -535,15 +535,8 @@ function VideoProvider(this: HTML5Provider, _playerId: string, _playerConfig: Ge
                     currentQuality = i;
                 }
                 if (label && levels[i].label === label) {
-                    currentQuality = i;
-                    break;
+                    return i;
                 }
-            }
-            if (!currentQuality) {
-                // Choose the highest quality
-                const sorted = levels.map((level, index) => Object.assign({ index, height: 0 }, level))
-                    .sort((a, b) => b.height - a.height);
-                currentQuality = sorted[0].index;
             }
         }
         visualQuality.reason = 'initial choice';
