31984488641c8bbb0d7c8df23a7034b4c39a52b2
Joe Natalzia <jnatalzia@jwplayer.com>
2020-03-05 17:26:06 -0500

    TS more things

--- src/js/api/api-queue.ts
+++ src/js/api/api-queue.ts
@@ -1,15 +1,27 @@
+import { PlayerAPI } from 'types/generic.type';
 
-export default function ApiQueueDecorator(instance, queuedCommands, predicate) {
-    const commandQueue = [];
+type CommandType = {
+    command: string;
+    args: Array<any>;
+}
+
+interface PlayerAPIQueue {
+    flush: () => void;
+    empty: () => void;
+    off: () => void;
+    destroy: () => void;
+    queue: Array<CommandType>;
+}
+
+export default function ApiQueueDecorator(this: PlayerAPIQueue, instance: PlayerAPI, queuedCommands: Array<string>, predicate: () => boolean): void {
+    const commandQueue: Array<CommandType> = [];
     const undecoratedMethods = {};
 
     queuedCommands.forEach((command) => {
         const method = instance[command];
         undecoratedMethods[command] = method;
 
-        instance[command] = function() {
-            const args = Array.prototype.slice.call(arguments, 0);
-
+        instance[command] = function(...args: Array<any>): void {
             if (predicate()) {
                 commandQueue.push({ command, args });
             } else {
@@ -21,27 +33,27 @@ export default function ApiQueueDecorator(instance, queuedCommands, predicate) {
         };
     });
 
-    function executeQueuedCommands() {
+    function executeQueuedCommands(): void {
         while (commandQueue.length > 0) {
-            const { command, args } = commandQueue.shift();
+            const { command, args } = commandQueue.shift() as CommandType;
             (undecoratedMethods[command] || instance[command]).apply(instance, args);
         }
     }
 
     Object.defineProperty(this, 'queue', {
         enumerable: true,
-        get: function() {
+        get: function(): Array<CommandType> {
             return commandQueue;
         }
     });
 
     this.flush = executeQueuedCommands;
 
-    this.empty = function() {
+    this.empty = function(): void {
         commandQueue.length = 0;
     };
 
-    this.off = function() {
+    this.off = function(): void {
         queuedCommands.forEach((command) => {
             const method = undecoratedMethods[command];
             if (method) {
@@ -51,7 +63,7 @@ export default function ApiQueueDecorator(instance, queuedCommands, predicate) {
         });
     };
 
-    this.destroy = function() {
+    this.destroy = function(): void {
         this.off();
         this.empty();
     };
