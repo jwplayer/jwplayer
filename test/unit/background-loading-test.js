aaab17450586872170954e5a882f77b07fbd2c0b
Rob Walch <rob@jwplayer.com>
2020-03-18 19:06:45 -0400

    Update bg loading test to use different media controllers

--- test/unit/background-loading-test.js
+++ test/unit/background-loading-test.js
@@ -180,12 +180,15 @@ describe('Background Loading', function () {
         it('destroys background media if there is an active foreground item', function () {
             programController._setActiveMedia(mediaController);
             programController.backgroundActiveMedia();
-            programController._setActiveMedia(mediaController);
+            const mockProvider2 = new MockProvider();
+            mockProvider2.video = document.createElement('video');
+            const mediaController2 = new MediaController(mockProvider2, model);
+            programController._setActiveMedia(mediaController2);
             programController._setActiveMedia = sinon.spy();
             mediaPool.recycle = sinon.spy();
 
             programController.restoreBackgroundMedia();
-            expect(programController._setActiveMedia.calledOnce).to.equal(false);
+            expect(programController._setActiveMedia.calledOnce).to.equal(false, 'called _setActiveMedia');
             expect(mediaPool.recycle.calledOnce).to.equal(true);
             expect(programController.background.currentMedia).to.equal(null);
         });
