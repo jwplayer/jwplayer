023b47e17892ba40a8ee3dd857f01975b174394b
Joe Natalzia <jnatalzia@gmail.com>
2020-03-27 10:44:52 -0400

    Promise return type

--- src/js/plugins/loader.ts
+++ src/js/plugins/loader.ts
@@ -4,12 +4,14 @@ import type { PlayerAPI, GenericObject, PluginObj } from 'types/generic.type';
 import type SimpleModel from 'model/simplemodel';
 import type PluginModel from './model';
 
+type LoadPromiseType = Promise<PluginObj | PlayerError | void>;
+
 export interface PluginLoaderInt {
-    load: (api: PlayerAPI, pluginsModel: PluginModel, pluginsConfig: GenericObject, model: SimpleModel) => PluginObj | PlayerError;
+    load: (api: PlayerAPI, pluginsModel: PluginModel, pluginsConfig: GenericObject, model: SimpleModel) => LoadPromiseType;
 }
 
 const PluginLoader = function (this: PluginLoaderInt): void {
-    this.load = function (api: PlayerAPI, pluginsModel: PluginModel, pluginsConfig: GenericObject, model: SimpleModel): PluginObj | PlayerError {
+    this.load = function (api: PlayerAPI, pluginsModel: PluginModel, pluginsConfig: GenericObject, model: SimpleModel): LoadPromiseType {
         // Must be a hash map
         if (!pluginsConfig || typeof pluginsConfig !== 'object') {
             return Promise.resolve();
