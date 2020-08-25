253285cced754cd8003a70f9348bdce59fdb46b3
Maria Maddox <mmaddox@jwplayer.com>
2020-08-25 16:20:26 -0400

    Add tizen-seekbar, thumbnailSeekbarTemplate, and seekbar controls

--- src/js/view/controls/tizen/tizen-controls.ts
+++ src/js/view/controls/tizen/tizen-controls.ts
@@ -1,10 +1,11 @@
 import Title from 'view/title';
 import TizenControlbar from 'view/controls/tizen/tizen-controlbar';
+import TizenSeekbar from './tizen-seekbar';
 import DisplayContainer from 'view/controls/display-container';
 import PauseDisplayTemplate from 'view/controls/tizen/templates/pause-display';
 import NextUpToolTip from 'view/controls/nextuptooltip';
 import SettingsMenu from 'view/controls/components/menu/settings-menu.js';
-import { addClass, createElement } from 'utils/dom';
+import { addClass, removeClass, createElement } from 'utils/dom';
 import { STATE_PLAYING, STATE_PAUSED } from 'events/events';
 import Controls from 'view/controls/controls';
 import type ViewModel from 'view/view-model';
@@ -26,6 +27,8 @@ class TizenControls extends Controls {
     pauseDisplay: HTMLElement | null;
     displayContainer: DisplayContainer | null;
     controlbar: TizenControlbar | null;
+    seekbar: TizenSeekbar | null;
+    seekState: boolean;
     settingsMenu: SettingsMenu | null;
     showing: boolean;
     instreamState: boolean;
@@ -45,6 +48,8 @@ class TizenControls extends Controls {
         this.pauseDisplay = null;
         this.displayContainer = null;
         this.controlbar = null;
+        this.seekbar = null;
+        this.seekState = false;
         this.settingsMenu = null;
         this.showing = false;
         this.instreamState = false;
@@ -107,6 +112,42 @@ class TizenControls extends Controls {
 
         this.div.appendChild(controlbar.element());
 
+        // Seekbar
+        const seekbar = this.seekbar = new TizenSeekbar(model, api, this.controlbar.elements.time);
+        this.div.appendChild(seekbar.element());
+
+        const enterSeekMode = () => {
+            if (this.seekbar) {
+                addClass(this.playerContainer, 'jw-flag-seek');
+                this.seekState = true;
+                this.seekbar.show();
+                api.pause();
+            }
+        };
+
+        const exitSeekMode = () => {
+            if (this.seekbar) {
+                removeClass(this.playerContainer, 'jw-flag-seek');
+                this.seekState = false;
+                this.seekbar.hide();
+            }
+        };
+
+        const updateSeek = (increment: number) => {
+            if (this.seekbar) {
+                this.seekbar.update(increment);
+            }
+        };
+
+        const seek = () => {
+            if (this.seekbar) {
+                this.seekbar.seek();
+                exitSeekMode();
+                api.play();
+                this.userInactive();
+            }
+        };
+
         // Settings/Tracks Menu
         const localization = model.get('localization');
         this.settingsMenu = new SettingsMenu(api, model.player, this.controlbar, localization);
@@ -117,30 +158,73 @@ class TizenControls extends Controls {
             }
             switch (evt.keyCode) {
                 case 37: // left-arrow
+                    if (this.seekState) {
+                        updateSeek(-10);
+                    } else if (!this.showing ||
+                        (this.controlbar && this.controlbar.activeButton === this.controlbar.elements.play)) {
+                        enterSeekMode();
+                    }
+                    this.userActive();
+                    break;
                 case 39: // right-arrow
+                    if (this.seekState) {
+                        updateSeek(10);
+                    } else if (!this.showing ||
+                        (this.controlbar && this.controlbar.activeButton === this.controlbar.elements.play)) {
+                        enterSeekMode();
+                    }
+                    this.userActive();
+                    break;
                 case 38: // up-arrow
+                    if (this.seekState) {
+                        exitSeekMode();
+                        this.userInactive();
+                        api.play();
+                        return;
+                    }
+                    this.userActive();
+                    break;
                 case 40: // down-arrow
+                    if (this.seekState) {
+                        exitSeekMode();
+                    }
                     this.userActive();
                     break;
                 case 13: // center/enter
                     evt.preventDefault();
+                    if (this.seekState) {
+                        seek();
+                        return;
+                    }
                     if (!this.showing) {
                         this.userActive();
                         api.playToggle(reasonInteraction());
                     }
                     break;
                 case 415: // play
+                    if (this.seekState) {
+                        seek();
+                        return;
+                    }
                     if (model.get('state') !== STATE_PLAYING) {
                         api.play();
                     }
                     break;
                 case 19: // pause
+                    if (this.seekState) {
+                        exitSeekMode();
+                        return;
+                    }
                     if (model.get('state') !== STATE_PAUSED) {
                         this.userActive();
                         api.pause();
                     }
                     break;
                 case 10252: // play/pause
+                    if (this.seekState) {
+                        seek();
+                        return;
+                    }
                     if (model.get('state') !== STATE_PAUSED) {
                         this.userActive();
                     }
@@ -151,6 +235,11 @@ class TizenControls extends Controls {
                 case 417: // FastForward
                     break;
                 case 10009: // Back
+                    if (this.seekState) {
+                        exitSeekMode();
+                        this.userActive();
+                        return;
+                    }
                     this.onBackClick(api);
                     break;
                 case 10182: // Exit/Home
