73c0ffcf91f424ccfede5c02249fd82569de054c
zetagame <zhaigh@jwplayer.com>
2021-01-27 12:39:50 -0500

    Update annotation in src/js/providers/utils/quality-labels.ts
    Co-authored-by: Rob Walch <robwalch@users.noreply.github.com>

--- src/js/providers/utils/quality-labels.ts
+++ src/js/providers/utils/quality-labels.ts
@@ -10,8 +10,7 @@ export function generateLabel(level?: ProviderLevel, qualityLabels?: QualityLabe
     if (!level) {
         return '';
     }
-    // flash provider uses bitrate instead of bandwidth
-    // Because these may be used by custom providers, they were not removed as part of flash deprecation
+    // custom providers may use level bitrate rather than bandwidth
     const bandwidth = level.bitrate || level.bandwidth;
     // flash provider, in some cases, will create its own label. Prefer it over creating a new label
     return getCustomLabel(qualityLabels, bandwidth) ||
