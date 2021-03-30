40e3d32c19e3d3c94078161cde5a107b4a2c1e7b
Rob Walch <rob@jwplayer.com>
2021-03-30 12:37:40 -0400

    [JW8-11548] Snap to edges in "uniform" stretching mode only when dimensions change by less than 6 pixels

--- src/js/providers/video-actions-mixin.ts
+++ src/js/providers/video-actions-mixin.ts
@@ -43,12 +43,17 @@ const VideoActionsMixin: VideoActionsInt = {
             width: '',
             height: ''
         };
+        const aspectPlayer = width / height;
+        const aspectVideo = videoWidth / videoHeight;
         if (stretching === 'uniform') {
-            // Snap video to edges when the difference in aspect ratio is less than 9% and perceivable
-            const playerAspectRatio = width / height;
-            const videoAspectRatio = videoWidth / videoHeight;
-            const edgeMatch = Math.abs(playerAspectRatio - videoAspectRatio);
-            if (edgeMatch < 0.09 && edgeMatch > 0.0025) {
+            // Snap video to edges to eliminate letterboxing of less than 3px on either edge
+            let letterBarPixels;
+            if (aspectPlayer > aspectVideo) {
+                letterBarPixels = width - width / (aspectPlayer / aspectVideo);
+            } else {
+                letterBarPixels = height - height / (aspectVideo / aspectPlayer);
+            }
+            if (letterBarPixels < 6) {
                 styles.objectFit = 'fill';
                 stretching = 'exactfit';
             }
@@ -61,8 +66,6 @@ const VideoActionsMixin: VideoActionsInt = {
         if (fitVideoUsingTransforms) {
             if (stretching !== 'uniform') {
                 styles.objectFit = 'contain';
-                const aspectPlayer = width / height;
-                const aspectVideo = videoWidth / videoHeight;
                 // Use transforms to center and scale video in container
                 let scaleX = 1;
                 let scaleY = 1;
