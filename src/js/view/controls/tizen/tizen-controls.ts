cd57cf7eb427bdf8274939ac425932731d681c68
Maria Maddox <mmaddox@jwplayer.com>
2021-05-07 11:38:46 -0400

    Reorder when super enable is called and when controls are set

--- src/js/view/controls/tizen/tizen-controls.ts
+++ src/js/view/controls/tizen/tizen-controls.ts
@@ -78,48 +78,36 @@ class TizenControls extends Controls {
     }
 
     enable(api: PlayerAPI, model: ViewModel): void {
-        super.enable.call(this, api, model);
-
         addClass(this.playerContainer, 'jw-tizen-app jw-flag-fullscreen');
         this.api = api;
         this.model = model;
 
         const element = this.context.createElement('div');
         element.className = 'jw-tizen-controls jw-tizen-reset';
-        this.div = element;
-    
-        if (!this.backdrop) {
-            const backdrop = this.context.createElement('div');
-            backdrop.className = 'jw-controls-backdrop jw-reset';
-            this.backdrop = backdrop;
-            this.addBackdrop();
-        }
 
         // Pause Display
         if (!this.pauseDisplay) {
             const pauseDisplay = createElement(PauseDisplayTemplate());
             const title = new Title(model);
             title.setup(pauseDisplay.querySelector('.jw-pause-display-container'));
-            this.div.appendChild(pauseDisplay);
+            element.appendChild(pauseDisplay);
             this.pauseDisplay = pauseDisplay;
         }
 
         // Display Buttons - Buffering
-        if (!this.displayContainer || !this.div.querySelector('.jw-display')) {
+        if (!this.displayContainer) {
             const displayContainer = new DisplayContainer(model, api);
             createBufferIcon(this.context, displayContainer, 'jw-tizen-buffer-draw');
             createBufferIcon(this.context, displayContainer, 'jw-tizen-buffer-erase');
 
-            this.div.appendChild(displayContainer.element());
+            element.appendChild(displayContainer.element());
             this.displayContainer = displayContainer;
         }
 
         // Controlbar
-        const controlbar = this.controlbar = new TizenControlbar(api, model,
+        const controlbar = new TizenControlbar(api, model,
             this.playerContainer.querySelector('.jw-hidden-accessibility'));
-        controlbar.on('backClick', () => {
-            this.onBackClick();
-        });
+        controlbar.on('backClick', this.onBackClick, this);
 
         // Next Up Tooltip
         if (model.get('nextUpDisplay') && !controlbar.nextUpToolTip) {
@@ -131,32 +119,30 @@ class TizenControls extends Controls {
             controlbar.nextUpToolTip = nextUpToolTip;
 
             // NextUp needs to be behind the controlbar to not block other tooltips
-            this.div.appendChild(nextUpToolTip.element());
+            element.appendChild(nextUpToolTip.element());
         }
 
-        this.div.appendChild(controlbar.element());
+        element.appendChild(controlbar.element());
 
         // Seekbar
-        const seekbar = this.seekbar = new TizenSeekbar(model, api, this.controlbar.elements.time);
-        this.div.appendChild(seekbar.element());
+        const seekbar = this.seekbar = new TizenSeekbar(model, api, controlbar.elements.time);
+        element.appendChild(seekbar.element());
 
         // Settings/Tracks Menu
         const localization = model.get('localization');
         const settingsMenu = this.settingsMenu = new TizenMenu(api, model.player, this.controlbar, localization);
         settingsMenu.on(USER_ACTION, () => this.userActive());
-        this.controlbar.on('settingsInteraction', () => {
+        controlbar.on('settingsInteraction', () => {
             settingsMenu.toggle();
             const activeButton = this.div && this.div.querySelector('.jw-active');
             if (settingsMenu.visible && activeButton) {
                 removeClass(activeButton, 'jw-active');
             }
         });
-        this.div.insertBefore(settingsMenu.el, controlbar.element());
+        element.insertBefore(settingsMenu.el, controlbar.element());
 
         // Trigger backClick when all videos complete      
-        api.on('playlistComplete', () => {
-            this.onBackClick();
-        }, this);
+        api.on('playlistComplete', this.onBackClick, this);
 
         // Remove event listener added in base controls
         if (this.keydownCallback) {
@@ -168,6 +154,12 @@ class TizenControls extends Controls {
         this.keydownCallback = (evt) => this.handleKeydown(evt);
         document.addEventListener('keydown', this.keydownCallback);
 
+        // To enable features like the ad skip button
+        super.enable.call(this, api, model);
+        this.controlbar = controlbar;
+        this.div = element;
+
+        this.addBackdrop();
         this.addControls();
         this.playerContainer.focus({ preventScroll: true });
 
