3390b0052ebdcd78945d8afda28bde250b373872
zetagame <zhaigh@jwplayer.com>
2021-06-01 13:54:25 -0500

    Re-implement directSelect for addClickAction

--- src/js/view/utils/add-click-action.ts
+++ src/js/view/utils/add-click-action.ts
@@ -1,7 +1,7 @@
 import UI from 'utils/ui';
 
 export function addClickAction(element: HTMLElement, clickAction: (evt: Event) => void, ctx?: any): UI {
-    const ui = new UI(element).on('click tap enter', clickAction, ctx);
+    const ui = new UI(element, { directSelect: true }).on('click tap enter', clickAction, ctx);
     
     return ui;
 }
