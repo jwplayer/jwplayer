c46a6b617ccb5693defa508dcdcc1824f0c24fa4
zetagame <zhaigh@jwplayer.com>
2021-06-08 11:53:49 -0500

    Remove directSelect from addClickAction

--- src/js/view/utils/add-click-action.ts
+++ src/js/view/utils/add-click-action.ts
@@ -1,7 +1,7 @@
 import UI from 'utils/ui';
 
-export function addClickAction(element: HTMLElement, clickAction: (evt: Event) => void, ctx?: any, directSelect?: boolean): UI {
-    const ui = new UI(element, { directSelect: !!directSelect }).on('click enter', clickAction, ctx);
+export function addClickAction(element: HTMLElement, clickAction: (evt: Event) => void, ctx?: any): UI {
+    const ui = new UI(element).on('click enter', clickAction, ctx);
     
     return ui;
 }
