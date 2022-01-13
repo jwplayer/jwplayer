3659e6366b21652ad5c5be1faec91db652f5a6e4
Jack <95260843+jdunningjwp@users.noreply.github.com>
2022-01-13 10:52:09 -0800

    fix: The next up display should work even with the PiP button is present in firefox [JW8-12112] (#3960)

--- src/js/view/controls/templates/nextup.ts
+++ src/js/view/controls/templates/nextup.ts
@@ -1,9 +1,13 @@
 import type { HTMLTemplateString } from 'types/generic.type';
+import { Browser } from 'environment/environment';
 
 export default (header = '', title = '', duration = '', closeAriaLabel = ''): HTMLTemplateString => {
+    // When using firefox this adds opacity to stop the firefox PiP button getting clicked instead of the next up div
+    const firefoxPipFix = Browser.firefox ? ' jw-nextup-firefox-pip-fix' : '';
+
     return (
-        `<div class="jw-nextup jw-background-color jw-reset">` +
-             `<div class="jw-nextup-tooltip jw-reset">` +
+        `<div class="jw-nextup jw-background-color jw-reset${firefoxPipFix}">` +
+                `<div class="jw-nextup-tooltip jw-reset">` +
                 `<div class="jw-nextup-thumbnail jw-reset"></div>` +
                 `<div class="jw-nextup-body jw-reset">` +
                     `<div class="jw-nextup-header jw-reset">${header}</div>` +
